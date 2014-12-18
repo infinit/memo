6a66504c6a3b28d8a91f8cea477cf7c3b85e6b0e
Matthieu Nottale <matthieu.nottale@infinit.io>
2014-12-18 21:55:40 +0100

    model.steg: Handle junk at end of data.

--- src/infinit/model/steg/Steg.cc
+++ src/infinit/model/steg/Steg.cc
@@ -1,5 +1,9 @@
+#include <arpa/inet.h>
+
 #include <boost/random.hpp>
 #include <boost/filesystem/fstream.hpp>
+
+#include <reactor/filesystem.hh>
 
 #include <infinit/model/steg/Steg.hh>
 #include <infinit/model/MissingBlock.hh>
@@ -7,7 +11,17 @@
 #include <cryptography/hash.hh>
 #include <elle/system/Process.hh>
 
-ELLE_LOG_COMPONENT("infinit.steg");
+extern "C" void outguess_extract(
+  u_char* key, int key_length, // secret key
+  u_char** out, u_int* out_len, // output extracted/decrypted data
+  char* filename // input image
+  );
+
+extern "C" int outguess_inject(u_char* data, int len, // data to embed
+  u_char* key, int key_length,  // secret key
+  char* filename // input/output image
+  );
+ELLE_LOG_COMPONENT("infinit.fs.steg");
 namespace infinit
 {
   namespace model
@@ -16,6 +30,15 @@ namespace infinit
     {
       
 namespace bfs = boost::filesystem;
+
+class InjectionFailure: public reactor::filesystem::Error
+{
+public:
+  InjectionFailure()
+  : reactor::filesystem::Error(EIO, "injection failure")
+  {}
+};
+
 
 Steg::Steg(boost::filesystem::path const& storage, std::string const& pass)
 : _storage_path(storage)
@@ -29,6 +52,7 @@ Steg::Steg(boost::filesystem::path const& storage, std::string const& pass)
     bfs::path p(*it);
     if (p.extension() == ".jpg" || p.extension() == ".JPG")
     {
+      ELLE_DEBUG("caching %s", p);
       _free_blocks.push_back(p);
       auto hash = cryptography::hash::sha256(p.string());
       Address res = Address(hash.contents());
@@ -91,30 +115,21 @@ Steg::__store(blocks::Block& block)
 {
   if (block.data().size() == 0)
     return;
+  elle::Buffer b;
+  b.append("steg", 4);
+  int32_t size = block.data().size();
+  size = htonl(size);
+  b.append(&size, 4);
+  b.append(block.data().contents(), block.data().size());
   auto it = _cache.find(block.address());
   if (it == _cache.end())
     throw MissingBlock(block.address());
-  auto tmpData = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
-  auto tmpImage = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
-  tmpImage += ".jpg";
-  bfs::ofstream ofs(tmpData);
-  ofs.write((const char*)block.data().contents(), block.data().size());
-  ofs.close();
-  // outguess -k secret -d m1K.txt orig.jpg out.jpg
-  ELLE_DEBUG("payload: %s, %s bytes", tmpData, block.data().size());
-  std::vector<std::string> args = {
-    "outguess", "-k", _passphrase,
-    "-d", tmpData.string(),
-    it->second.string(),
-    tmpImage.string()
-  };
-  elle::system::Process p(args);
-  p.wait();
-  ELLE_DEBUG("pre: %s", bfs::file_size(tmpImage));
-  bfs::rename(tmpImage, it->second);
-  ELLE_DEBUG("%s -> %s", tmpImage, it->second);
-  ELLE_DEBUG("post: %s", bfs::file_size(it->second));
-  bfs::remove(tmpData);
+  int res = outguess_inject(b.mutable_contents(), b.size(),
+                  (unsigned char*)_passphrase.c_str(), _passphrase.length(),
+                  (char*)it->second.string().c_str());
+  if (res < 0)
+    throw InjectionFailure();
+  ELLE_TRACE("Injected %s bytes from %s", b.size(), it->second);
 }
 
 std::unique_ptr<blocks::Block>
@@ -122,6 +137,7 @@ Steg::_fetch(Address address) const
 {
   if (!_root)
   {
+    ELLE_DEBUG("First fetch: %x", address);
     _root = address;
     _root_data = __fetch(address);
     Address redirect(_root_data->data().contents());
@@ -151,29 +167,29 @@ Steg::__fetch(Address address) const
       _free_blocks.pop_back();
     }
   }
-  auto tmpData = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
-  //outguess -k secret -r out.jpg recover.txt
-  std::vector<std::string> args = {
-    "outguess", "-k", _passphrase,
-    "-r", it->second.string(),
-    tmpData.string()
-  };
-  elle::system::Process p(args);
-  p.wait();
-  elle::Buffer data;
-  bfs::ifstream ifs(tmpData);
-  char buffer[1024];
-  while (true)
+  unsigned char* d;
+  unsigned int dlen;
+  outguess_extract((unsigned char*)_passphrase.c_str(), _passphrase.length(),
+                   &d, &dlen,
+                   (char*)it->second.string().c_str());
+  ELLE_TRACE("Extracted %s bytes from %s", dlen, it->second);
+  if (memcmp("steg", d, 4))
   {
-    ifs.read(buffer, 1024);
-    int cnt = ifs.gcount();
-    if (cnt <= 0)
-      break;
-    data.append(buffer, cnt);
+    ELLE_WARN("Corrupted data");
+    free(d);
+    return elle::make_unique<blocks::Block>(address);
   }
+  int32_t size = *((int32_t*)d + 1);
+  size = ntohl(size);
+  if (size > dlen - 8)
+    throw std::runtime_error("Short read");
+  if (size < dlen - 8)
+    ELLE_WARN("Too much data: %s > %s", dlen-8, size);
+  elle::Buffer data;
+  data.append(d+8, size);
+  free(d);
   ELLE_DEBUG("fetched %s: %s bytes", it->second, data.size());
   auto res = elle::make_unique<blocks::Block>(address, std::move(data));
-  bfs::remove(tmpData);
   return res;
 }
 
