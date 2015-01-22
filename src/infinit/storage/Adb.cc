499c83628f9fa0cf6a8c68c19d5066f4a04bbe68
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-01-22 15:34:43 +0100

    Storage: android adb backend.
    ...or how to tell the bearclaw is bored.

---
+++ src/infinit/storage/Adb.cc
@@ -0,0 +1,100 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <elle/system/Process.hh>
+#include <infinit/storage/Adb.hh>
+
+static const char* tmpIn = "/tmp/adb_file_in";
+static const char* tmpOut = "/tmp/adb_file_out";
+
+namespace infinit
+{
+  namespace storage
+  {
+    Adb::Adb(std::string const& root)
+    :_root(root)
+    {}
+    elle::Buffer Adb::_get(Key key) const
+    {
+      std::string remote = elle::sprintf("%s/%x", _root, key);
+      std::vector<std::string> args = {
+        "adb",
+        "pull",
+        remote,
+        tmpIn
+      };
+      elle::system::Process p(args);
+      p.wait();
+      struct stat st;
+      ::stat(tmpIn, &st);
+      std::ifstream ifs(tmpIn, std::ios::binary);
+      elle::Buffer res;
+      res.size(st.st_size);
+      ifs.read((char*)res.mutable_contents(), st.st_size);
+      unlink(tmpIn);
+      return res;
+    }
+
+    void Adb::_set(Key key, elle::Buffer const& value, bool insert, bool update)
+    {
+      std::string remote = elle::sprintf("%s/%x", _root, key);
+      std::ofstream ofs(tmpOut);
+      ofs.write((char*)value.contents(), value.size());
+      ofs.close();
+      std::vector<std::string> args = {
+        "adb",
+        "push",
+        tmpOut,
+        remote
+      };
+      elle::system::Process p(args);
+      p.wait();
+      unlink(tmpOut);
+    }
+
+    void Adb::_erase(Key key)
+    {
+      std::string remote = elle::sprintf("%s/%x", _root, key);
+      std::vector<std::string> args = {
+        "adb",
+        "shell",
+        "rm",
+        remote
+      };
+      elle::system::Process p(args);
+      p.wait();
+    }
+
+    struct AdbStorageConfig:
+    public StorageConfig
+    {
+    public:
+      std::string root;
+      std::shared_ptr<StorageConfig> storage;
+      AdbStorageConfig(elle::serialization::SerializerIn& input)
+      : StorageConfig()
+      {
+        this->serialize(input);
+      }
+
+      void
+      serialize(elle::serialization::Serializer& s)
+      {
+        s.serialize("root", this->root);
+      }
+
+      virtual
+      std::unique_ptr<infinit::storage::Storage>
+      make() override
+      {
+        return elle::make_unique<infinit::storage::Adb>(root);
+      }
+    };
+
+    static const elle::serialization::Hierarchy<StorageConfig>::
+    Register<AdbStorageConfig>
+    _register_AdbStorageConfig("adb");
+  }
+}
---
+++ src/infinit/storage/Adb.hh
@@ -0,0 +1,31 @@
+#ifndef INFINIT_STORAGE_ADB_HH
+# define INFINIT_STORAGE_ADB_HH
+
+# include <infinit/storage/Key.hh>
+# include <infinit/storage/Storage.hh>
+
+namespace infinit
+{
+  namespace storage
+  {
+    class Adb
+      : public Storage
+    {
+    public:
+      Adb(std::string const& root);
+    protected:
+      virtual
+      elle::Buffer
+      _get(Key k) const override;
+      virtual
+      void
+      _set(Key k, elle::Buffer const& value, bool insert, bool update) override;
+      virtual
+      void
+      _erase(Key k);
+      ELLE_ATTRIBUTE(std::string, root);
+    };
+  }
+}
+
+#endif
