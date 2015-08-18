0215ef4c050932b83fed1db1dd28fec67707ab19
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-08-18 11:09:34 +0200

    storage.Async: merge operations on same block by default.

--- src/infinit/storage/Async.cc
+++ src/infinit/storage/Async.cc
@@ -11,18 +11,26 @@ namespace infinit
 {
   namespace storage
   {
-    Async::Async(std::unique_ptr<Storage> backend, int max_blocks, int64_t max_size)
+    Async::Async(std::unique_ptr<Storage> backend,
+                 int max_blocks,
+                 int64_t max_size,
+                 bool merge)
       : _backend(std::move(backend))
       , _max_blocks(max_blocks)
       , _max_size(max_size)
       , _thread("async deque", [&]{this->_worker();})
+      , _op_offset(0)
       , _blocks(0)
       , _bytes(0)
+      , _merge(merge)
     {
       _queueing.open();
     }
     Async::~Async()
     {
+      if (!_op_cache.empty())
+        ELLE_ERR("ASync filter deleted with %s operations still in cache!",
+                 _op_cache.size());
       ELLE_TRACE("~Async");
       _thread.terminate_now();
     }
@@ -45,12 +53,38 @@ namespace infinit
     }
 
     void
+    Async::_push_op(Key k, elle::Buffer const& buf, Operation op)
+    {
+      if (_merge)
+      {
+        auto it = _op_index.find(k);
+        if (it != _op_index.end())
+        {
+          auto index = it->second;
+          ELLE_ASSERT(index >= _op_offset);
+          ELLE_ASSERT(index < _op_offset + _op_cache.size());
+          auto& prev = _op_cache[index - _op_offset];
+          ELLE_ASSERT_EQ(std::get<0>(prev), k);
+          _dec(std::get<1>(prev).size());
+          std::get<1>(prev).reset();
+          std::get<1>(prev).append(buf.contents(), buf.size());
+          std::get<2>(prev) = op;
+          _inc(buf.size());
+          return;
+        }
+      }
+      _op_cache.emplace_back(k, elle::Buffer(buf), op);
+      _inc(buf.size());
+      if (_merge)
+        _op_index.insert(std::make_pair(k, _op_offset + _op_cache.size()-1));
+    }
+
+    void
     Async::_erase(Key k)
     {
       ELLE_DEBUG("queueing erase on %x", k);
       _wait();
-      _op_cache.emplace_back(k, elle::Buffer(), Operation::erase);
-      _inc(0);
+      _push_op(k, elle::Buffer(), Operation::erase);
     }
 
     void
@@ -58,8 +92,7 @@ namespace infinit
     {
       _wait();
       ELLE_DEBUG("queueing set on %x . Cache blocks=%s  bytes=%s", k, _blocks, _bytes);
-      _op_cache.emplace_back(k, elle::Buffer(value), Operation::set);
-      _inc(value.size());
+      _push_op(k, value, Operation::set);
     }
 
     std::vector<Key>
@@ -83,10 +116,23 @@ namespace infinit
                      (op == Operation::erase) ? "erase" : "set",
                      k, _blocks, _bytes);
           _op_cache.pop_front();
+          ++_op_offset;
+          if (_merge)
+            _op_index.erase(k);
           _dec(buf.size());
           if (op == Operation::erase)
-            _backend->erase(k);
-          else
+          { // if we merged a set and an erase and the set was a 'create',
+            // erase might legitimaly fail
+            try
+            {
+              _backend->erase(k);
+            }
+            catch(MissingKey const& mk)
+            {
+              ELLE_TRACE("Erase failed with %s", mk);
+            }
+          }
+          else if (op == Operation::set)
             _backend->set(k, buf, true, true);
         }
       }
@@ -131,11 +177,14 @@ namespace infinit
       std::unique_ptr<Storage> backend = instantiate(args[0], args[1]);
       int max_blocks = 100;
       int64_t max_bytes = -1;
+      bool merge = true;
       if (args.size() > 2)
         max_blocks = std::stoi(args[2]);
       if (args.size() > 3)
         max_bytes = std::stol(args[3]);
-      return elle::make_unique<Async>(std::move(backend), max_blocks, max_bytes);
+      if (args.size() > 4)
+        merge = std::stol(args[4]);
+      return elle::make_unique<Async>(std::move(backend), max_blocks, max_bytes, merge);
     }
 
     struct AsyncStorageConfig:
@@ -144,6 +193,7 @@ namespace infinit
     public:
       int64_t max_blocks;
       int64_t max_size;
+      boost::optional<bool> merge;
       std::shared_ptr<StorageConfig> storage;
       AsyncStorageConfig(elle::serialization::SerializerIn& input)
       : StorageConfig()
@@ -156,6 +206,7 @@ namespace infinit
       {
         s.serialize("max_blocks", this->max_blocks);
         s.serialize("max_size", this->max_size);
+        s.serialize("merge", this->merge);
         s.serialize("backend", this->storage);
       }
 
@@ -164,7 +215,8 @@ namespace infinit
       make() override
       {
         return elle::make_unique<infinit::storage::Async>(
-          std::move(storage->make()), max_blocks, max_size);
+          std::move(storage->make()), max_blocks, max_size,
+          merge ? *merge : true);
       }
     };
 
--- src/infinit/storage/Async.hh
+++ src/infinit/storage/Async.hh
@@ -14,7 +14,7 @@ namespace infinit
     class Async: public Storage
     {
     public:
-      Async(std::unique_ptr<Storage> backend, int max_blocks = 100, int64_t max_size = -1);
+      Async(std::unique_ptr<Storage> backend, int max_blocks = 100, int64_t max_size = -1, bool merge = true);
       ~Async();
     protected:
       virtual
@@ -30,25 +30,32 @@ namespace infinit
       std::vector<Key>
       _list() override;
     private:
+      enum class Operation
+      {
+        erase,
+        set,
+        none
+      };
+
       void _worker();
       void _inc(int64_t size);
       void _dec(int64_t size);
       void _wait(); /// Wait for cache to go bellow limit
+      void _push_op(Key k, elle::Buffer const& buf, Operation op);
       std::unique_ptr<Storage> _backend;
       int _max_blocks;
       int64_t _max_size;
       reactor::Barrier _dequeueing;
       reactor::Barrier _queueing;
       reactor::Thread _thread;
-      enum class Operation
-      {
-        erase,
-        set
-      };
+
       typedef std::tuple<Key, elle::Buffer, Operation> Entry;
       std::deque<Entry> _op_cache;
+      unsigned long _op_offset; // number of popped elements
       int _blocks;
       int _bytes;
+      bool _merge; // merge ops to have at most one per key in cache
+      std::unordered_map<Key, unsigned long> _op_index;
     };
   }
 }
