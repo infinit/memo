0aa3fbab53e561b8781b45d13257ce613ade2b64
mefyl <mefyl@infinit.io>
2015-09-03 11:39:45 +0200

    website: Reload automatically, for real.

--- website/src/__main__.py
+++ website/src/__main__.py
@@ -2,4 +2,4 @@ import bottle
 import infinit.website
 
 bottle.DEBUG = True
-bottle.run(app = infinit.website.Website(), autoreload = True)
+bottle.run(app = infinit.website.Website(), reloader = True)
