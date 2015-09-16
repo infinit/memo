c4088a50d2094793c26eed77c362aa20bad0f25c
mefyl <mefyl@infinit.io>
2015-09-16 13:54:58 +0200

    model.doughnut: Factor unit test.

--- tests/doughnut.cc
+++ tests/doughnut.cc
@@ -19,49 +19,64 @@ ELLE_LOG_COMPONENT("infinit.model.doughnut.test");
 namespace dht = infinit::model::doughnut;
 namespace storage = infinit::storage;
 
+class DHTs
+{
+public:
+  DHTs()
+    : keys_a(infinit::cryptography::rsa::keypair::generate(2048))
+    , keys_b(infinit::cryptography::rsa::keypair::generate(2048))
+  {
+    this->local_a = std::make_shared<dht::Local>(
+      elle::make_unique<storage::Memory>());
+    this->local_b = std::make_shared<dht::Local>(
+      elle::make_unique<storage::Memory>());
+    dht::Passport passport_a(keys_a.K(), "network-name", keys_a.k());
+    dht::Passport passport_b(keys_b.K(), "network-name", keys_a.k());
+    infinit::overlay::Stonehenge::Hosts members;
+    members.push_back(local_a->server_endpoint());
+    members.push_back(local_b->server_endpoint());
+    this->dht_a = std::make_shared<dht::Doughnut>(
+      keys_a,
+      keys_a.K(),
+      passport_a,
+      static_cast<infinit::model::doughnut::Doughnut::OverlayBuilder>(
+        [=](infinit::model::doughnut::Doughnut*d) {
+          return elle::make_unique<infinit::overlay::Stonehenge>(
+            elle::UUID::random(), members, d);
+        }),
+      boost::filesystem::path(".")
+      );
+    local_a->doughnut() = dht_a.get();
+    dht_a->overlay()->register_local(local_a);
+    local_a->serve();
+    this->dht_b = std::make_shared<dht::Doughnut>(
+      keys_b,
+      keys_a.K(),
+      passport_b,
+      static_cast<infinit::model::doughnut::Doughnut::OverlayBuilder>(
+        [=](infinit::model::doughnut::Doughnut*d) {
+          return elle::make_unique<infinit::overlay::Stonehenge>(
+            elle::UUID::random(), members, d);
+        }),
+      boost::filesystem::path(".")
+      );
+    local_b->doughnut() = dht_b.get();
+    dht_b->overlay()->register_local(local_b);
+    local_b->serve();
+  }
+
+  infinit::cryptography::rsa::KeyPair keys_a;
+  infinit::cryptography::rsa::KeyPair keys_b;
+  std::shared_ptr<dht::Local> local_a;
+  std::shared_ptr<dht::Local> local_b;
+  std::shared_ptr<dht::Doughnut> dht_a;
+  std::shared_ptr<dht::Doughnut> dht_b;
+};
+
 ELLE_TEST_SCHEDULED(doughnut)
 {
-  auto local_a = std::make_shared<dht::Local>(
-    elle::make_unique<storage::Memory>());
-  auto local_b = std::make_shared<dht::Local>(
-    elle::make_unique<storage::Memory>());
-  auto keys_a = infinit::cryptography::rsa::keypair::generate(2048);
-  dht::Passport passport_a(keys_a.K(), "network-name", keys_a.k());
-  auto keys_b = infinit::cryptography::rsa::keypair::generate(2048);
-  dht::Passport passport_b(keys_b.K(), "network-name", keys_a.k());
-  infinit::overlay::Stonehenge::Hosts members;
-  members.push_back(local_a->server_endpoint());
-  members.push_back(local_b->server_endpoint());
-
-  auto dht_a = std::make_shared<dht::Doughnut>(
-    keys_a,
-    keys_a.K(),
-    passport_a,
-    static_cast<infinit::model::doughnut::Doughnut::OverlayBuilder>(
-      [=](infinit::model::doughnut::Doughnut*d) {
-        return elle::make_unique<infinit::overlay::Stonehenge>(
-          elle::UUID::random(), members, d);
-      }),
-    boost::filesystem::path(".")
-    );
-  local_a->doughnut() = dht_a.get();
-  dht_a->overlay()->register_local(local_a);
-  local_a->serve();
-  auto dht_b = std::make_shared<dht::Doughnut>(
-    keys_b,
-    keys_a.K(),
-    passport_b,
-    static_cast<infinit::model::doughnut::Doughnut::OverlayBuilder>(
-      [=](infinit::model::doughnut::Doughnut*d) {
-        return elle::make_unique<infinit::overlay::Stonehenge>(
-          elle::UUID::random(), members, d);
-      }),
-    boost::filesystem::path(".")
-    );
-  local_b->doughnut() = dht_b.get();
-  local_b->serve();
-  dht_b->overlay()->register_local(local_b);
-  auto& dht = *dht_a;
+  DHTs dhts;
+  auto& dht = *dhts.dht_a;
   {
     elle::Buffer data("\\_o<", 4);
     auto block = dht.make_block<infinit::model::blocks::ImmutableBlock>(data);
@@ -93,84 +108,42 @@ ELLE_TEST_SCHEDULED(doughnut)
 
 ELLE_TEST_SCHEDULED(ACB)
 {
-  auto local_a = std::make_shared<dht::Local>(
-    elle::make_unique<storage::Memory>());
-  auto local_b = std::make_shared<dht::Local>(
-    elle::make_unique<storage::Memory>());
-  auto keys_a = infinit::cryptography::rsa::keypair::generate(2048);
-  dht::Passport passport_a(keys_a.K(), "network-name", keys_a.k());
-  auto keys_b = infinit::cryptography::rsa::keypair::generate(2048);
-  dht::Passport passport_b(keys_b.K(), "network-name", keys_a.k());
-  infinit::overlay::Stonehenge::Hosts members;
-  members.push_back(local_a->server_endpoint());
-  members.push_back(local_b->server_endpoint());
-  auto dht_a = std::make_shared<dht::Doughnut>(
-    keys_a,
-    keys_a.K(),
-    passport_a,
-    static_cast<infinit::model::doughnut::Doughnut::OverlayBuilder>(
-      [=](infinit::model::doughnut::Doughnut*d) {
-        return elle::make_unique<infinit::overlay::Stonehenge>(
-          elle::UUID::random(), members, d);
-      }),
-    boost::filesystem::path(".")
-    );
-  local_a->doughnut() = dht_a.get();
-  dht_a->overlay()->register_local(local_a);
-  local_a->serve();
-  auto dht_b = std::make_shared<dht::Doughnut>(
-    keys_b,
-    keys_a.K(),
-    passport_b,
-    static_cast<infinit::model::doughnut::Doughnut::OverlayBuilder>(
-      [=](infinit::model::doughnut::Doughnut*d) {
-        return elle::make_unique<infinit::overlay::Stonehenge>(
-          elle::UUID::random(), members, d);
-      }),
-    boost::filesystem::path(".")
-    );
-  local_b->doughnut() = dht_b.get();
-  dht_b->overlay()->register_local(local_b);
-  local_b->serve();
-  auto& dht = *dht_a;
-  auto& other_dht = *dht_b;
+  DHTs dhts;
+  auto block = dhts.dht_a->make_block<infinit::model::blocks::ACLBlock>();
+  elle::Buffer data("\\_o<", 4);
+  block->data(elle::Buffer(data));
+  ELLE_LOG("owner: store ACB")
+    dhts.dht_a->store(*block);
   {
-    auto block = dht.make_block<infinit::model::blocks::ACLBlock>();
-    elle::Buffer data("\\_o<", 4);
-    block->data(elle::Buffer(data));
-    ELLE_LOG("owner: store ACB")
-      dht.store(*block);
-    {
-      ELLE_LOG("other: fetch ACB");
-      auto fetched = other_dht.fetch(block->address());
-      // No read permissions.
-      // BOOST_CHECK_THROW(fetched->data(), elle::Error);
-      auto acb = elle::cast<infinit::model::blocks::ACLBlock>::runtime(fetched);
-      acb->data(elle::Buffer(":-(", 3));
-      ELLE_LOG("other: stored edited ACB")
-        // FIXME: slice
-        BOOST_CHECK_THROW(other_dht.store(*acb), elle::Exception);
-        // BOOST_CHECK_THROW(other_dht.store(*acb),
-        //                   dht::ValidationFailed);
-    }
-    ELLE_LOG("owner: add ACB permissions")
-      block->set_permissions(dht::User(keys_b.K(), ""), true, true);
-    ELLE_LOG("owner: store ACB")
-      dht.store(*block);
-    {
-      ELLE_LOG("other: fetch ACB");
-      auto fetched = other_dht.fetch(block->address());
-      BOOST_CHECK_EQUAL(fetched->data(), "\\_o<");
-      auto acb = elle::cast<infinit::model::blocks::ACLBlock>::runtime(fetched);
-      acb->data(elle::Buffer(":-)", 3));
-      ELLE_LOG("other: stored edited ACB")
-        other_dht.store(*acb);
-    }
-    ELLE_LOG("owner: fetch ACB")
-    {
-      auto fetched = dht.fetch(block->address());
-      BOOST_CHECK_EQUAL(fetched->data(), ":-)");
-    }
+    ELLE_LOG("other: fetch ACB");
+    auto fetched = dhts.dht_b->fetch(block->address());
+    // No read permissions.
+    // BOOST_CHECK_THROW(fetched->data(), elle::Error);
+    auto acb = elle::cast<infinit::model::blocks::ACLBlock>::runtime(fetched);
+    acb->data(elle::Buffer(":-(", 3));
+    ELLE_LOG("other: stored edited ACB")
+      // FIXME: slice
+      BOOST_CHECK_THROW(dhts.dht_b->store(*acb), elle::Exception);
+      // BOOST_CHECK_THROW(dhts.dht_a->store(*acb),
+      //                   dht::ValidationFailed);
+  }
+  ELLE_LOG("owner: add ACB permissions")
+    block->set_permissions(dht::User(dhts.keys_b.K(), ""), true, true);
+  ELLE_LOG("owner: store ACB")
+    dhts.dht_a->store(*block);
+  {
+    ELLE_LOG("other: fetch ACB");
+    auto fetched = dhts.dht_b->fetch(block->address());
+    BOOST_CHECK_EQUAL(fetched->data(), "\\_o<");
+    auto acb = elle::cast<infinit::model::blocks::ACLBlock>::runtime(fetched);
+    acb->data(elle::Buffer(":-)", 3));
+    ELLE_LOG("other: stored edited ACB")
+      dhts.dht_b->store(*acb);
+  }
+  ELLE_LOG("owner: fetch ACB")
+  {
+    auto fetched = dhts.dht_a->fetch(block->address());
+    BOOST_CHECK_EQUAL(fetched->data(), ":-)");
   }
 }
 
