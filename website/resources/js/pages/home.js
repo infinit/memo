f222796780fc756c2e494ca4fb9c1c1af3eb7952
Gaëtan Rochel <gaetan.rochel@infinit.io>
2015-10-28 14:23:44 +0100

    website: Update terminal commands.

--- website/resources/js/pages/home.js
+++ website/resources/js/pages/home.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
   $('body.home #commands').t(
-    '<ins>1 </ins><mark>$></mark> infinit-storage --create —-aws-s3 -—account 455444556 -—name s3 --contribution 10TB' +
+    '<ins>1 </ins><mark>$></mark> infinit-storage --create —-aws-s3 -—name s3 --contribution 10TB' +
     '<ins>0.5</ins><ins>\nCreated storage "s3". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-network --create —-storage s3 -—storage local —-name hybrid-cloud --replication-factor 3' +
+    'infinit-network --create —-storage s3 -—storage local —-name hybrid-cloud' +
     '<ins>0.5</ins><ins>\nCreated network "hybrid-cloud". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
