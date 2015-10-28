2f6c8afdeddb3b3be630cea9b2feb171a658129e
Gaëtan Rochel <gaetan.rochel@infinit.io>
2015-10-28 14:42:43 +0100

    website: Update global schema.

--- website/resources/js/pages/home.js
+++ website/resources/js/pages/home.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
   $('body.home #commands').t(
-    '<ins>1 </ins><mark>$></mark> infinit-storage --create —-aws-s3 -—name s3 --contribution 10TB' +
+    '<ins>1 </ins><mark>$></mark> infinit-storage --create --aws-s3 --name s3 --contribution 10TB' +
     '<ins>0.5</ins><ins>\nCreated storage "s3". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-network --create —-storage s3 -—storage local —-name hybrid-cloud' +
+    'infinit-network --create --storage s3 --storage local --name hybrid-cloud' +
     '<ins>0.5</ins><ins>\nCreated network "hybrid-cloud". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-volume —-create -—network hybrid-cloud —-name company' +
+    'infinit-volume --create --network hybrid-cloud --name company' +
     '<ins>0.5</ins><ins>\nStarting network "hybrid-cloud".'+
     '\nCreating volume root blocks.'+
     '\nCreated volume "company". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-volume -—mount /mnt/company/ -—name company' +
+    'infinit-volume --mount /mnt/company/ --name company' +
     '<ins>0.5</ins><ins>\nMounting volume "company". \n\n</ins>'+
 
     '<mark>$></mark> ',
