f9ef7abc3394e74ce9efca21357134d7ec7b1959
Julien Quintard <julien.quintard@infinit.io>
2015-10-28 14:16:49 +0100

    Change the title/subtitle and command-line example.

--- website/resources/js/pages/home.js
+++ website/resources/js/pages/home.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
   $('body.home #commands').t(
-    '<ins>1 </ins><mark>$></mark> infinit-storage --create —dropbox —account 455444556 —name dropbox' +
-    '<ins>0.5</ins><ins>\nCreated storage “dropbox". \n\n</ins>' +
+    '<ins>1 </ins><mark>$></mark> infinit-storage --create —-aws-s3 -—account 455444556 -—name s3 --contribution 10TB' +
+    '<ins>0.5</ins><ins>\nCreated storage "s3". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-network —create —storage dropbox —storage flickr —name clouds' +
-    '<ins>0.5</ins><ins>\nCreated network "clouds". \n\n</ins>' +
+    'infinit-network --create —-storage s3 -—storage local —-name hybrid-cloud --replication-factor 3' +
+    '<ins>0.5</ins><ins>\nCreated network "hybrid-cloud". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-volume —create —network clouds —name personal-drive' +
-    '<ins>0.5</ins><ins>\nStarting network "clouds".'+
+    'infinit-volume —-create -—network hybrid-cloud —-name company' +
+    '<ins>0.5</ins><ins>\nStarting network "hybrid-cloud".'+
     '\nCreating volume root blocks.'+
-    '\nCreated volume “personal-drive". \n\n</ins>' +
+    '\nCreated volume "company". \n\n</ins>' +
     '<mark>$></mark> <ins>2</ins>'+
 
-    'infinit-volume —mount —name personal-drive —mountpoint /mnt/personal-drive/' +
-    '<ins>0.5</ins><ins>\nMounting volume “personal-drive". \n\n</ins>'+
+    'infinit-volume -—mount /mnt/company/ -—name company' +
+    '<ins>0.5</ins><ins>\nMounting volume "company". \n\n</ins>'+
 
     '<mark>$></mark> ',
     {
