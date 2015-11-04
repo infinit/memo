212b81a2cd0347a36e39b8b201898e0959ecdfed
Gaëtan Rochel <gaetan.rochel@infinit.io>
2015-11-04 18:06:53 +0100

    website: Add Google Analytics.

--- website/resources/js/pages/pricing.js
+++ website/resources/js/pages/pricing.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
 
+  function updatePrice() {
+
+  }
+
   $("#usersInput").bind("slider:changed", function (event, data) {
-    console.log(data.value);
     var users = $('.estimation table .users td span');
+    var usersLabel = $('#usersLabel span');
     users.text(data.value);
+    usersLabel.text(data.value);
+  });
+
+  $("#capacityInput").bind("slider:changed", function (event, data) {
+    var capacities = $('.estimation table .capacity td span');
+    var capacityLabel = $('#capacityLabel span');
+    capacities.text(data.value);
+    capacityLabel.text(data.value);
   });
 
 });
