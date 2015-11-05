7062c1eeef1608288c5a77307b146c4e6897a24d
Gaëtan Rochel <gaetan.rochel@infinit.io>
2015-11-05 15:07:58 +0100

    website: Update estimation pricing with logos.

--- website/resources/js/pages/pricing.js
+++ website/resources/js/pages/pricing.js
@@ -2,25 +2,23 @@ $(document).ready(function() {
 
   var users = $('.estimation table .users td span');
   var usersLabel = $('#usersLabel span');
-  var capacities = $('.estimation table .capacity td span');
+  var capacities = $('.estimation table tr th');
   var capacityLabel = $('#capacityLabel span');
   var prices = $('.estimation table .price td span');
-  var capacityInfinit = $('.estimation table .capacity td.infinit span');
 
   // Price Update
   function updatePrice() {
     $.each(prices, function(key, element) {
-      var service = $(element).parent().attr('class');
-      var el = $(users).parent().filter('.' + service);
-      var current_users = $(el).find('span').text();
+      var service = $(element).parent().attr('data-service');
+      var el = $(users).parent().filter("[data-service='" + service + "']");
+      var current_users = $(usersLabel).text();
       var price = $(el).find('span').text();
       var new_price;
 
       new_price = $(element).attr('data-price') * 12 * current_users;
 
       if (service === 'infinit') {
-        new_price = new_price + ($(capacityInfinit).text() * 0.03 * 2);
-        console.log($(capacityInfinit).text(), new_price);
+        new_price = new_price + ($(capacityLabel).text() * 0.03 * 2);
       }
 
       $(element).text(new_price);
@@ -29,22 +27,26 @@ $(document).ready(function() {
 
   // Users Slider
   $("#usersInput").bind("slider:changed", function (event, data) {
-    users.text(data.value);
     usersLabel.text(data.value);
-
     updatePrice();
   });
 
   // Capacity Slider
   $("#capacityInput").bind("slider:changed", function (event, data) {
+
     $.each(capacities, function(key, element) {
-      if (!$(element).attr('data-max') || $(element).attr('data-max') >= data.value) {
-        $(element).text(data.value);
-      } else {
-        $(element).text($(element).attr('data-max'));
+      var service = $(element).attr('data-service');
+
+      if ($(element).attr('data-max')) {
+        if ($(element).attr('data-max') < data.value) {
+          $("[data-service='" + service + "']").addClass('disabled');
+        } else {
+          $("[data-service='" + service + "']").removeClass('disabled');
+        }
       }
     });
 
+    capacityLabel.text(data.value);
     updatePrice();
   });
 
