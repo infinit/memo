6dd4038d1d35a6561a9d01f443b9f9a5f47eec1f
Gaëtan Rochel <gaetan.rochel@infinit.io>
2015-11-05 16:38:36 +0100

    website: Update pricing with AeroFS.

--- website/resources/js/pages/pricing.js
+++ website/resources/js/pages/pricing.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var users = $('.estimation table .users td span');
   var usersLabel = $('#usersLabel span');
-  var capacities = $('.estimation table tr th');
+  var services = $('.estimation table tr th');
   var capacityLabel = $('#capacityLabel span');
   var prices = $('.estimation table .price td span');
 
@@ -11,14 +11,25 @@ $(document).ready(function() {
     $.each(prices, function(key, element) {
       var service = $(element).parent().attr('data-service');
       var el = $(users).parent().filter("[data-service='" + service + "']");
-      var current_users = $(usersLabel).text();
-      var price = $(el).find('span').text();
-      var new_price;
+      var current_users = parseInt($(usersLabel).text());
+      var price_per_user = $(element).attr('data-price');
+      var price_per_gb = $(element).attr('data-price-gb');
+      var free_until = parseInt($(element).attr('data-free-until'));
+      var start_at = parseInt($(element).attr('data-start-at'));
+      var new_price = 0;
 
-      new_price = $(element).attr('data-price') * 12 * current_users;
+      if (service === 'egnyte' && current_users > 24) {
+        price_per_user = $(element).attr('data-price-max');
+      }
 
-      if (service === 'infinit') {
-        new_price = new_price + ($(capacityLabel).text() * 0.03 * 2);
+      new_price =  price_per_user * 12 * current_users;
+
+      if (free_until) {
+        if (current_users >= free_until) {
+          new_price = new_price + ($(capacityLabel).text() * price_per_gb * 2);
+        } else {
+          new_price = $(capacityLabel).text() * price_per_gb * 2;
+        }
       }
 
       $(element).text(new_price);
@@ -28,20 +39,33 @@ $(document).ready(function() {
   // Users Slider
   $("#usersInput").bind("slider:changed", function (event, data) {
     usersLabel.text(data.value);
+
+    $.each(services, function(key, element) {
+      var service = $(element).attr('data-service');
+
+      if ($(element).attr('data-start-at')) {
+        if ($(element).attr('data-start-at') > data.value) {
+          $("[data-service='" + service + "']").addClass('users_disabled');
+        } else {
+          $("[data-service='" + service + "']").removeClass('users_disabled');
+        }
+      }
+    });
+
     updatePrice();
   });
 
   // Capacity Slider
   $("#capacityInput").bind("slider:changed", function (event, data) {
 
-    $.each(capacities, function(key, element) {
+    $.each(services, function(key, element) {
       var service = $(element).attr('data-service');
 
-      if ($(element).attr('data-max')) {
-        if ($(element).attr('data-max') < data.value) {
-          $("[data-service='" + service + "']").addClass('disabled');
+      if ($(element).attr('data-max-capacity')) {
+        if ($(element).attr('data-max-capacity') < data.value) {
+          $("[data-service='" + service + "']").addClass('capacity_disabled');
         } else {
-          $("[data-service='" + service + "']").removeClass('disabled');
+          $("[data-service='" + service + "']").removeClass('capacity_disabled');
         }
       }
     });
@@ -49,5 +73,7 @@ $(document).ready(function() {
     capacityLabel.text(data.value);
     updatePrice();
   });
+
+  updatePrice();
 
 });
