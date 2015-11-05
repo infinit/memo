709e224ef2eb0b0db37072fc1aa04ac0b593c0a6
Gaëtan Rochel <gaetan.rochel@infinit.io>
2015-11-05 18:14:46 +0100

    website: Update pricing.

--- website/resources/js/pages/doc.js
+++ website/resources/js/pages/doc.js
@@ -17,55 +17,61 @@ $(document).ready(function() {
     }
   });
 
-  var a = function () {
-    var height = $(window).scrollTop();
-    var menu_anchor = $("#menu-anchor").offset().top - 13;
-    var footer = $("footer").offset().top;
-    var menu = $("ul.menu");
-    var menu_height = $("ul.menu").height() + 60; // margin bottom
+  if ($('body').hasClass('documentation')) {
 
-    if (height > menu_anchor) {
-      var myTop = $(window).scrollTop();
+    var a = function () {
+      var height = $(window).scrollTop();
+      var menu_anchor = $("#menu-anchor").offset().top - 13;
+      var footer = $("footer").offset().top;
+      var menu = $("ul.menu");
+      var menu_height = $("ul.menu").height() + 60; // margin bottom
 
-      if (myTop > footer - menu_height) {
-        myTop = footer - menu_height;
-        menu.css({
-          position: "absolute",
-          top: myTop,
-          bottom: ""
-        });
+      if (height > menu_anchor) {
+        var myTop = $(window).scrollTop();
+
+        if (myTop > footer - menu_height) {
+          myTop = footer - menu_height;
+          menu.css({
+            position: "absolute",
+            top: myTop,
+            bottom: ""
+          });
+
+        } else {
+          menu.css({
+            position: "fixed",
+            top: '14px',
+            bottom: ""
+          });
+        }
 
       } else {
         menu.css({
-          position: "fixed",
-          top: '14px',
+          position: "absolute",
+          top: "",
           bottom: ""
         });
       }
+    };
 
-    } else {
-      menu.css({
-        position: "absolute",
-        top: "",
-        bottom: ""
-      });
-    }
-  };
-
-  $(window).scroll(a);
+    $(window).scroll(a);
+  }
 
 
-  var winHeight = $(window).height(),
-      docHeight = $(document).height(),
-      progressBar = $('progress'),
-      max, value;
+  if ($('body').hasClass('doc_get_started')) {
 
-  max = docHeight - winHeight - $("footer").height();
-  progressBar.attr('max', max);
+    var winHeight = $(window).height(),
+        docHeight = $(document).height(),
+        progressBar = $('progress'),
+        max, value;
 
-  $(document).on('scroll', function(){
-     value = $(window).scrollTop();
-     progressBar.attr('value', value);
-  });
+    max = docHeight - winHeight - $("footer").height();
+    progressBar.attr('max', max);
+
+    $(document).on('scroll', function(){
+       value = $(window).scrollTop();
+       progressBar.attr('value', value);
+    });
+  }
 
 });
