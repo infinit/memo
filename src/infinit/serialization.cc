d1a2a20074fa425d0b653faf0e32e8311077d65b
mefyl <mefyl@infinit.io>
2015-12-16 12:45:54 +0100

    serialization: Include dependencies versions.

--- src/infinit/serialization.cc
+++ src/infinit/serialization.cc
@@ -5,4 +5,9 @@ namespace infinit
 {
   elle::Version serialization_tag::version(
     INFINIT_MAJOR, INFINIT_MINOR, INFINIT_SUBMINOR);
+  std::unordered_map<elle::Version, elle::serialization::Serializer::Versions>
+  serialization_tag::dependencies{{
+    { elle::Version(0, 3, 0),
+      {{ elle::type_info<elle::serialization_tag>(), elle::Version(0, 0, 0) }}},
+  }};
 }
--- src/infinit/serialization.hh
+++ src/infinit/serialization.hh
@@ -2,12 +2,15 @@
 # define INFINIT_SERIALIZATION_HH
 
 # include <elle/Version.hh>
+# include <elle/serialization/Serializer.hh>
 
 namespace infinit
 {
   struct serialization_tag
   {
     static elle::Version version;
+    static std::unordered_map<
+      elle::Version, elle::serialization::Serializer::Versions> dependencies;
   };
 }
 
