4e3eba8fa8d00b0691a403cf49bad477d5910476
Christopher Crone <christopher.crone@infinit.io>
2015-12-18 19:15:06 +0100

    bin.credentials: Add AWS. Tidy up.

--- bin/infinit-credentials.cc
+++ bin/infinit-credentials.cc
@@ -9,21 +9,62 @@ using namespace boost::program_options;
 
 infinit::Infinit ifnt;
 
+static const boost::regex _aws_access_key_regex("[A-Z0-9]{20}");
+static const boost::regex _aws_secrect_key_regex("[A-Za-z0-9/+=]{40}");
+
+static
+std::string
+read_key(std::string const& prompt_text, boost::regex const& regex)
+{
+  boost::smatch matches;
+  std::string res;
+  {
+    bool first = true;
+    while (!boost::regex_match(res, matches, regex))
+    {
+      if (!first)
+      {
+        std::cout << "Invalid \"" << prompt_text << "\", try again..."
+                  << std::endl;
+      }
+      else
+        first = false;
+      std::cout << prompt_text << ": ";
+      std::cout.flush();
+      std::getline(std::cin, res);
+    }
+  }
+  return res;
+}
+
 COMMAND(add)
 {
+  auto user = self_user(ifnt, args);
   if (args.count("dropbox"))
   {
-    auto user = self_user(ifnt, args);
     std::cout << "Register your Dropbox account with infinit by visiting "
               << beyond() << "/users/" << user.name
               << "/dropbox-oauth" << std::endl;
   }
   else if (args.count("google"))
   {
-    auto user = ifnt.user_get(get_username(args, "user"));
     std::cout << "Register your Google account with infinit by visiting "
               << beyond() << "/users/" << user.name
               << "/google-oauth" << std::endl;
+  }
+  else if (args.count("aws"))
+  {
+    auto account = mandatory(args, "account", "account name");
+    std::cout << "Please enter your AWS credentials" << std::endl;
+    std::string access_key_id =
+      read_key("Access Key ID", _aws_access_key_regex);
+    std::string secret_access_key =
+      read_key("Secret Access Key", _aws_secrect_key_regex);
+    auto aws_credentials = elle::make_unique<infinit::AWSCredentials>(
+      account, access_key_id, secret_access_key);
+    ifnt.credentials_aws_add(std::move(aws_credentials));
+    report_action(
+      "stored", "AWS credentials", account, std::string("locally"));
   }
   else
     throw CommandLineError("service type not specified");
@@ -31,6 +72,7 @@ COMMAND(add)
 
 struct Enabled
 {
+  bool aws;
   bool dropbox;
   bool google;
   bool multi;
@@ -39,58 +81,36 @@ struct Enabled
 Enabled
 enabled(variables_map const& args)
 {
+  int aws = args.count("aws") ? 1 : 0;
   int dropbox = args.count("dropbox") ? 1 : 0;
   int google = args.count("google") ? 1 : 0;
-  if (!dropbox && !google)
-    dropbox = google = 1;
-  return Enabled { bool(dropbox), bool(google), dropbox + google > 1 };
+  if (!aws && !dropbox && !google)
+    aws = dropbox = google = 1;
+  return Enabled {
+    bool(aws), bool(dropbox), bool(google), aws + dropbox + google > 1 };
 }
 
+template <typename T>
 void
 fetch_credentials(infinit::User const& user,
                   std::string const& name,
                   std::string const& pretty,
-                  std::function<void (Credentials)> add)
+                  std::function<void (std::unique_ptr<T>)> add)
 {
-  using namespace infinit::cryptography;
-  std::string where = elle::sprintf(
-    "users/%s/credentials/%s",
-    user.name, name);
-  elle::Buffer string_to_sign("GET;", 4);
-  string_to_sign.append(where.data(), where.size());
-  string_to_sign.append(";", 1);
-  auto payload_hash = hash(elle::ConstWeakBuffer(),
-                           Oneway::sha256);
-  auto pl64 = elle::format::base64::encode(payload_hash);
-  string_to_sign.append(pl64.contents(), pl64.size());
-
-  string_to_sign.append(";", 1);
-  auto now = std::to_string(time(0));
-  string_to_sign.append(now.data(), now.size());
-  auto sig = user.private_key->sign(
-    string_to_sign,
-    infinit::cryptography::rsa::Padding::pkcs1,
-    infinit::cryptography::Oneway::sha256);
-  auto sig64 = elle::format::base64::encode(sig);
-  reactor::http::Request::Configuration c;
-  c.header_add("infinit-signature", sig64.string());
-  c.header_add("infinit-time", now);
-  reactor::http::Request r(
-    elle::sprintf("%s/%s", beyond(), where),
-    reactor::http::Method::GET, std::move(c));
-  if (r.status() == reactor::http::StatusCode::Not_Found)
+  std::string where = elle::sprintf("users/%s/credentials/%s", user.name, name);
+  // FIXME: Workaround for using std::unique_ptr.
+  // Remove when serialization does not require copy.
+  auto res = beyond_fetch_json(
+    where, elle::sprintf("\"%s\" credentials for", name), user.name, user);
+  auto root = boost::any_cast<elle::json::Object>(res);
+  auto credentials_vec =
+      boost::any_cast<std::vector<elle::json::Json>>(root["credentials"]);
+  for (auto const& a_json: credentials_vec)
   {
-    throw elle::Error(elle::sprintf("user %s is not published",
-                                    user.name));
-  }
-  ELLE_DUMP("response: %s", r);
-  auto credentials =
-    elle::serialization::json::deserialize<std::vector<Credentials>>
-    (r, "credentials", false);
-  for (auto& a: credentials)
-  {
+    elle::serialization::json::SerializerIn input(a_json, false);
+    auto a = elle::make_unique<T>(input.deserialize<T>());
     elle::printf("Fetched %s credentials %s (%s)\n",
-                 pretty, a.uid, a.display_name);
+                 pretty, a->uid(), a->display_name());
     add(std::move(a));
   }
 }
@@ -98,15 +118,22 @@ fetch_credentials(infinit::User const& user,
 COMMAND(fetch)
 {
   auto e = enabled(args);
+  if (((!e.dropbox && !e.google) || (e.dropbox ^ e.google)) && e.aws)
+  {
+    throw CommandLineError(elle::sprintf("AWS credentials are not stored on %s",
+                           beyond(true)));
+  }
   auto user = self_user(ifnt, args);
   if (e.dropbox)
-    fetch_credentials(
+    fetch_credentials<infinit::OAuthCredentials>(
       user, "dropbox", "Dropbox",
-      [] (Credentials a) { ifnt.credentials_dropbox_add(std::move(a)); });
+      [] (std::unique_ptr<infinit::OAuthCredentials> a)
+      { ifnt.credentials_dropbox_add(std::move(a)); });
   if (e.google)
-    fetch_credentials(
+    fetch_credentials<infinit::OAuthCredentials>(
       user, "google", "Google Drive",
-      [] (Credentials a) { ifnt.credentials_google_add(std::move(a)); });
+      [] (std::unique_ptr<infinit::OAuthCredentials> a)
+      { ifnt.credentials_google_add(std::move(a)); });
   // FIXME: remove deleted ones
 }
 
@@ -135,6 +162,8 @@ namespace s                                                             \
   } Sym;                                                                \
 };                                                                      \
 
+SYMBOL(aws);
+SYMBOL(credentials_aws);
 SYMBOL(dropbox);
 SYMBOL(credentials_dropbox);
 SYMBOL(google);
@@ -142,7 +171,10 @@ SYMBOL(credentials_google);
 
 template <typename Service, typename Fetch>
 void
-list_(Enabled const& e, Service service, Fetch fetch)
+list_(Enabled const& e,
+      Service service,
+      Fetch fetch,
+      std::string const& service_name)
 {
   if (!service.get_attribute(e))
     return;
@@ -150,11 +182,11 @@ list_(Enabled const& e, Service service, Fetch fetch)
   for (auto const& credentials: fetch.call_method(ifnt))
   {
     if (e.multi && first)
-      std::cout << "Dropbox:" << std::endl;
+      std::cout << service_name << ":" << std::endl;
     if (e.multi)
       std::cout << "  ";
-    std::cout << credentials.uid << ": "
-              << credentials.display_name << std::endl;
+    std::cout << credentials->uid() << ": "
+              << credentials->display_name() << std::endl;
     first = false;
   }
 }
@@ -162,8 +194,9 @@ list_(Enabled const& e, Service service, Fetch fetch)
 COMMAND(list)
 {
   auto e = enabled(args);
-  list_(e, s::dropbox, s::credentials_dropbox);
-  list_(e, s::google, s::credentials_google);
+  list_(e, s::aws, s::credentials_aws, "AWS");
+  list_(e, s::dropbox, s::credentials_dropbox, "Dropbox");
+  list_(e, s::google, s::credentials_google, "Google");
 }
 
 int
@@ -173,6 +206,11 @@ main(int argc, char** argv)
   services_options.add_options()
     ("dropbox", "Dropbox account credentials")
     ("google", "Google account credentials")
+    ("aws", "Amazon Web Services account credentials")
+    ;
+  options_description aws_options("AWS account options");
+  aws_options.add_options()
+    ("account", value<std::string>(), "account name")
     ;
   program = argv[0];
   Modes modes {
@@ -182,7 +220,7 @@ main(int argc, char** argv)
       &add,
       "SERVICE",
       {},
-      {services_options},
+      {services_options, aws_options},
     },
     {
       "fetch",
