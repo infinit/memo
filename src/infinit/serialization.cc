5d80548175936a707a00d683f2cc5af6bec96eac
mefyl <mefyl@infinit.io>
2016-01-04 13:04:08 +0100

    Register 0.4.0 serialization.

--- src/infinit/serialization.cc
+++ src/infinit/serialization.cc
@@ -5,9 +5,11 @@ namespace infinit
 {
   elle::Version serialization_tag::version(
     INFINIT_MAJOR, INFINIT_MINOR, INFINIT_SUBMINOR);
-  std::unordered_map<elle::Version, elle::serialization::Serializer::Versions>
+  elle::unordered_map<elle::Version, elle::serialization::Serializer::Versions>
   serialization_tag::dependencies{{
     { elle::Version(0, 3, 0),
+      {{ elle::type_info<elle::serialization_tag>(), elle::Version(0, 0, 0) }}},
+    { elle::Version(0, 4, 0),
       {{ elle::type_info<elle::serialization_tag>(), elle::Version(0, 0, 0) }}},
   }};
 }
