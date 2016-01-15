db4f0275069ee462529bf6343b1b886c012098de
Gaëtan Rochel <gaetan.rochel@infinit.io>
2016-01-15 15:18:25 +0100

    website: Add Pricing page.

--- website/resources/js/pages/pricing.js
+++ website/resources/js/pages/pricing.js
@@ -1,76 +1,70 @@
 $(document).ready(function() {
 
-  var users = $('.estimation table .users td span');
   var usersLabel = $('#usersLabel span');
-  var services = $('.estimation table tr th');
   var capacityLabel = $('#capacityLabel span');
-  var prices = $('.estimation table .price td span');
 
   // Price Update
   function updatePrice() {
-    $.each(prices, function(key, element) {
-      var service = $(element).parent().attr('data-service');
-      var el = $(users).parent().filter("[data-service='" + service + "']");
-      var current_users = parseInt($(usersLabel).text());
-      var price_per_user = $(element).attr('data-price');
-      var price_per_gb = $(element).attr('data-price-gb');
-      var free_until = parseInt($(element).attr('data-free-until'));
-      var start_at = parseInt($(element).attr('data-start-at'));
-      var new_price = 0;
+    var current_users = parseInt($(usersLabel).text());
+    var current_storage = parseInt($(capacityLabel).text());
+    var price_per_gb = $('#storageSelect option:selected').attr('data-price');
+    var price_per_gb_aerofs = 0.13;
+    var max_users = $('.plan.startup p.max_users').attr('data-max-users');
+    var price_per_user = $('.plan.business p.price').attr('data-price');
+    var price_per_user_dropbox = 12;
+    var price_per_user_aerofs = 15;
+    var price = 0;
+    var price_dropbox = 0;
+    var price_aerofs = 0;
+    var porcentage;
 
-      if (service === 'egnyte' && current_users > 24) {
-        price_per_user = $(element).attr('data-price-max');
-      }
+    // yearly price
+    if (current_users >= max_users) {
+      price =  price_per_user * 12 * current_users;
+    }
 
-      new_price =  price_per_user * 12 * current_users;
+    // yearly price with storage
+    price += (current_storage * 1000 * price_per_gb);
 
-      if (free_until) {
-        if (current_users >= free_until) {
-          new_price = new_price + ($(capacityLabel).text() * 1000 * price_per_gb * 12);
-        } else {
-          new_price = $(capacityLabel).text() * 1000 * price_per_gb * 12;
-        }
-      }
+    // display aerofs comparison when > 30 users
+    // else display dropbox
+    if (current_users < 100 && (current_storage === 1 || current_storage === 0)) {
+      $('.price .service').text('Dropbox');
+      price_dropbox = price_per_user_dropbox * 12 * current_users;
+      porcentage = 100 - (price / price_dropbox * 100);
+    } else {
+      $('.price .service').text('AeroFS');
+      price_aerofs = price_per_user_aerofs * 12 * current_users;
+      price_aerofs += current_storage * 1000 * price_per_gb_aerofs;
+      porcentage = 100 - (price / price_aerofs * 100);
+    }
 
-      $(element).text(new_price);
-    });
+    $('.price .porcentage').text(Math.round(porcentage));
+
+    if (price === 0) {
+      $('.estimation p.free').show();
+      $('.estimation p.infinit').hide();
+    } else {
+      $('.estimation p.infinit span').text(price);
+      $('.estimation p.infinit').show();
+      $('.estimation p.free').hide();
+    }
+
   }
 
   // Users Slider
   $("#usersInput").bind("slider:changed", function (event, data) {
     usersLabel.text(data.value);
-
-    $.each(services, function(key, element) {
-      var service = $(element).attr('data-service');
-
-      if ($(element).attr('data-start-at')) {
-        if ($(element).attr('data-start-at') > data.value) {
-          $("[data-service='" + service + "']").addClass('users_disabled');
-        } else {
-          $("[data-service='" + service + "']").removeClass('users_disabled');
-        }
-      }
-    });
-
     updatePrice();
   });
 
   // Capacity Slider
   $("#capacityInput").bind("slider:changed", function (event, data) {
-
-    $.each(services, function(key, element) {
-      var service = $(element).attr('data-service');
-
-      if ($(element).attr('data-max-capacity')) {
-        if ($(element).attr('data-max-capacity') < data.value) {
-          $("[data-service='" + service + "']").addClass('capacity_disabled');
-        } else {
-          $("[data-service='" + service + "']").removeClass('capacity_disabled');
-        }
-      }
-    });
-
     capacityLabel.text(data.value);
+    updatePrice();
+  });
+
+  $('#storageSelect').on('change', function() {
     updatePrice();
   });
 
