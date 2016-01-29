7481efd538e56b40422315bd64301532688dd923
mefyl <mefyl@gruntech.org>
2016-01-29 13:30:10 +0100

    bin: Compile version separately.

---
+++ bin/version.cc
@@ -0,0 +1,7 @@
+#include <infinit/version.hh>
+
+#include "version.hh"
+
+const elle::Version version(
+  INFINIT_MAJOR, INFINIT_MINOR, INFINIT_SUBMINOR);
+const std::string version_describe(INFINIT_VERSION);
