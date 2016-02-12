599324871c0ca75a366989fc673446bf59d4bf32
mefyl <mefyl@infinit.io>
2016-02-12 09:13:35 +0100

    beyond: Honor 80 columns limit.

--- beyond/src/__init__.py
+++ beyond/src/__init__.py
@@ -165,7 +165,8 @@ class Beyond:
       raise ValueError('passphrase_hash')
     self.pairing_information_delete(owner)
     if self.now > pairing.expiration:
-      raise exceptions.NoLongerAvailable('%s pairing information' % owner)
+      raise exceptions.NoLongerAvailable(
+        '%s pairing information' % owner)
     return pairing
 
   def pairing_information_delete(self, owner):
@@ -175,7 +176,8 @@ class Beyond:
     json = self.__datastore.pairing_fetch(owner)
     pairing = PairingInformation.from_json(self, json)
     if self.now > pairing.expiration:
-      raise exceptions.NoLongerAvailable('%s pairing information' % owner)
+      raise exceptions.NoLongerAvailable(
+        '%s pairing information' % owner)
     return True
 
   ## ------- ##
@@ -189,11 +191,11 @@ class Beyond:
   def network_delete(self, owner, name):
     return self.__datastore.network_delete(owner = owner, name = name)
 
-  def network_volumes_get(self, network):
-    return self.__datastore.networks_volumes_fetch(networks = [network])
+  def network_volumes_get(self, name):
+    return self.__datastore.networks_volumes_fetch(networks = [name])
 
-  def network_stats_get(self, network):
-    return self.__datastore.network_stats_fetch(network = network)
+  def network_stats_get(self, name):
+    return self.__datastore.network_stats_fetch(network = name)
 
   ## ---- ##
   ## User ##
@@ -216,10 +218,11 @@ class Beyond:
     return self.__datastore.user_networks_fetch(user = user)
 
   def user_volumes_get(self, user):
-    # XXX: This requires two requests as we cannot combine results across
-    # databases.
+    # XXX: This requires two requests as we cannot combine results
+    # across databases.
     networks = self.__datastore.user_networks_fetch(user = user)
-    return self.__datastore.networks_volumes_fetch(networks = networks)
+    return self.__datastore.networks_volumes_fetch(
+      networks = networks)
 
   def user_drives_get(self, name):
     return self.__datastore.user_drives_fetch(name = name)
@@ -609,7 +612,8 @@ class Entity(type):
                         if getattr(self, f) is None and d is None),
                        None)
         if missing is not None:
-          raise exceptions.MissingField(type(self).__name__.lower(), missing)
+          raise exceptions.MissingField(
+            type(self).__name__.lower(), missing)
         getattr(self.__beyond._Beyond__datastore, insert)(self)
       content['create'] = create
     # Save
@@ -618,7 +622,8 @@ class Entity(type):
         diff = {}
         for field in fields:
           v = getattr(self, field)
-          original_field = '_%s__%s_original' % (self.__class__.__name__, field)
+          original_field = '_%s__%s_original' % (
+            self.__class__.__name__, field)
           original = getattr(self, original_field)
           if isinstance(v, dict):
             for k, v in v.items():
@@ -663,15 +668,17 @@ class Entity(type):
                update = None,
                fields = []):
     for f in fields:
-      content[f] = property(lambda self: getattr(self, '_%s__%s' % (name, f)))
+      content[f] = property(
+        lambda self: getattr(self, '_%s__%s' % (name, f)))
     type.__init__(self, name, superclasses, content)
 
 def fields(*args, **kwargs):
   return dict(chain(((k, None) for k in args), kwargs.items()))
 
-class PairingInformation(metaclass = Entity,
-                         insert = 'pairing_insert',
-                         fields = fields('name', 'passphrase_hash', 'data', 'expiration')):
+class PairingInformation(
+    metaclass = Entity,
+    insert = 'pairing_insert',
+    fields = fields('name', 'passphrase_hash', 'data', 'expiration')):
 
   @property
   def id(self):
@@ -692,8 +699,10 @@ class Network(metaclass = Entity,
     return self.name
 
   def __eq__(self, other):
-    if self.name != other.name or self.owner != other.owner or \
-        self.consensus != other.consensus or self.overlay != other.overlay:
+    if self.name != other.name or \
+       self.owner != other.owner or \
+       self.consensus != other.consensus or \
+       self.overlay != other.overlay:
       return False
     return True
 
@@ -723,24 +732,28 @@ class Volume(metaclass = Entity,
       return False
     return True
 
-class Drive(metaclass = Entity,
-            insert = 'drive_insert',
-            update = 'drive_update',
-            fields = fields('name', 'owner', 'network', 'volume', 'description',
-                            users = {})):
+class Drive(
+    metaclass = Entity,
+    insert = 'drive_insert',
+    update = 'drive_update',
+    fields = fields('name', 'owner', 'network',
+                    'volume', 'description', users = {})):
 
   @property
   def id(self):
     return self.name
 
   def __eq__(self, other):
-    if self.name != other.name or self.network != other.network or \
-        self.volume != other.volume or self.description != other.description:
+    if self.name != other.name or \
+       self.network != other.network or \
+       self.volume != other.volume or \
+       self.description != other.description:
       return False
     return True
 
-  class Invitation(metaclass = Entity,
-                   fields = fields('permissions', 'status', 'create_home')):
+  class Invitation(
+      metaclass = Entity,
+      fields = fields('permissions', 'status', 'create_home')):
     statuses = ['pending', 'ok']
 
     class AlreadyConfirmed(Exception):
@@ -771,19 +784,23 @@ class Drive(metaclass = Entity,
       drive.users[key] = self.json()
       drive.save()
       variables = {
-        'owner': { x: getattr(owner, x) for x in ['name', 'email'] },
-        'drive': { x: getattr(drive, x) for x in ['name', 'description'] },
+        'owner': {
+          x: getattr(owner, x) for x in ['name', 'email'] },
+        'drive':
+        { x: getattr(drive, x) for x in ['name', 'description'] },
       }
       if plain:
         variables['invitee'] = { 'email': email }
       else:
-        variables['invitee'] = { x: getattr(invitee, x) for x in ['name', 'email'] }
+        variables['invitee'] = {
+          x: getattr(invitee, x) for x in ['name', 'email'] }
         variables['invitee']['avatar'] = '/users/%s/avatar' % key
 
       variables['owner']['avatar'] = '/users/%s/avatar' % owner.name
       variables['drive']['icon'] = '/drives/%s/icon' % drive.name
       if invitation and email is not None:
-        template = 'Drive/Invitation' if not plain else 'Drive/Plain Invitation'
+        template = \
+          'Drive/%sInvitation' % ('' if not plain else 'Plain ')
         beyond.emailer.send_one(
           recipient_email = email,
           recipient_name = key,
