81691d43080f26017a05be26bfc2992b1cf76242
Antony MECHIN <antony.mechin@infinit.io>
2016-03-23 20:06:24 +0100

    infinit.model.doughnut.Cache: Add missing include.

--- src/infinit/model/doughnut/Cache.hh
+++ src/infinit/model/doughnut/Cache.hh
@@ -7,6 +7,7 @@
 # include <boost/multi_index_container.hpp>
 # include <boost/multi_index/hashed_index.hpp>
 # include <boost/multi_index/identity.hpp>
+# include <boost/multi_index/mem_fun.hpp>
 # include <boost/multi_index/ordered_index.hpp>
 # include <boost/multi_index/sequenced_index.hpp>
 
