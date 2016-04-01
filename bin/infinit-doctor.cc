3fd52cf47624c09f20b60825441e9bd2799f9984
Antony MECHIN <antony.mechin@infinit.io>
2016-04-01 14:23:32 +0200

    bin: Add infinit-doctor.

---
+++ bin/infinit-doctor.cc
@@ -0,0 +1,609 @@
+#include <unordered_map>
+#include <pair>
+#include <map>
+
+#include <elle/log.hh>
+#include <elle/log/TextLogger.hh>
+#include <elle/string/algorithm.hh>
+#include <elle/system/Process.hh>
+#include <elle/os/environ.hh>
+
+#include <infinit/storage/Dropbox.hh>
+#include <infinit/storage/Filesystem.hh>
+#include <infinit/storage/GCS.hh>
+#include <infinit/storage/GoogleDrive.hh>
+#include <infinit/storage/Strip.hh>
+#include <infinit/storage/sftp.hh>
+#include <infinit/storage/S3.hh>
+
+#include <connectivity/connectivity.hh>
+#include <reactor/network/upnp.hh>
+#include <reactor/scheduler.hh>
+
+ELLE_LOG_COMPONENT("infinit-doctor");
+
+#include <main.hh>
+
+infinit::Infinit ifnt;
+
+static
+std::string
+result(bool value)
+{
+  return value ? "✓" : "×";
+}
+
+// Return the infinit related environment.
+static
+std::unordered_map<std::string, std::string>
+infinit_related_environment()
+{
+  auto environ = elle::os::environ();
+  // Remove non INFINIT_ or ELLE_ prefixed entries.
+  for (auto it = environ.begin(); it != environ.end();)
+  {
+    if (it->first.find("INFINIT_") != 0 &&
+        it->first.find("ELLE_") != 0)
+      environ.erase(it++);
+    else
+      ++it;
+  }
+  return environ;
+}
+
+static
+bool
+_networking(boost::program_options::variables_map const& args,
+            boost::optional<std::ostream&> output_stream)
+{
+
+  bool sane = true;
+  // Contact beyond.
+  auto& output = std::cout;
+  output << "Contacting " << beyond() << std::endl;
+  {
+    try
+    {
+      reactor::http::Request r(beyond(), reactor::http::Method::GET);
+      reactor::wait(r);
+      if (r.status() != reactor::http::StatusCode::OK)
+        throw elle::Error(
+          elle::sprintf(
+            "Able to contact %s but got a %s error",
+            beyond(), r.status()));
+      output << "  ok" << std::endl << std::endl;
+    }
+    catch (elle::Error const&)
+    {
+      elle::fprintf(output, "Unable to contact %s: %s\n", beyond(),
+                    elle::exception_string());
+    }
+  }
+  // Interfaces.
+  auto interfaces = elle::network::Interface::get_map(
+    elle::network::Interface::Filter::no_loopback);
+  output << "Local IP Addresses:" << std::endl;
+  std::vector<std::string> public_ips;
+  for (auto i: interfaces)
+  {
+    if (i.second.ipv4_address.empty())
+      continue;
+    output << "  " << i.second.ipv4_address << std::endl;
+    public_ips.push_back(i.second.ipv4_address);
+  }
+
+  output << "\nConnectivity:" << std::endl;
+  std::string host = "192.241.139.66";
+  uint16_t port = 5456;
+  auto run = [&] (std::string const& name,
+                  std::function<reactor::connectivity::Result (
+                    std::string const& host,
+                    uint16_t port)> const& function)
+  {
+    std::string result = elle::sprintf("  %s: ", name);
+    try
+    {
+      auto address = function(host, port);
+      result += address.host;
+      if (std::find(public_ips.begin(), public_ips.end(), address.host) ==
+          public_ips.end())
+        result += " (external)";
+      else
+        result += " (internal)";
+      if (address.local_port)
+      {
+        result += " ";
+        if (address.local_port == address.remote_port)
+          result += "same port";
+        else
+          result += elle::sprintf("different ports (local: %s, remote: %s)",
+                               address.local_port, address.remote_port);
+      }
+    }
+    catch (...)
+    {
+      result += elle::exception_string();
+    }
+    output << result << std::endl;
+  };
+  run("TCP", reactor::connectivity::tcp);
+  run("UDP", reactor::connectivity::udp);
+  run("RDV_UTP", reactor::connectivity::rdv_utp);
+  output << "NAT ";
+  try
+  {
+    output << reactor::connectivity::nat(host, port) << std::endl;
+  }
+  catch (std::runtime_error const&)
+  {
+    output << elle::exception_string() << std::endl;
+  }
+
+  output << std::endl << "UPNP:" << std::endl;
+  auto upnp = reactor::network::UPNP::make();
+  try
+  {
+    upnp->initialize();
+    output << "  available: " << upnp->available() << std::endl;
+    auto ip = upnp->external_ip();
+    output << "  external_ip: " << ip << std::endl;
+    auto pm = upnp->setup_redirect(reactor::network::Protocol::tcp, 5678);
+    output << "  mapping: " << pm.internal_host << ':' << pm.internal_port
+      << " -> " << pm.external_host << ':' << pm.external_port << std::endl;
+    auto pm2 = upnp->setup_redirect(reactor::network::Protocol::udt, 5679);
+    output << "  mapping: " << pm2.internal_host << ':' << pm2.internal_port
+      << " -> " << pm2.external_host << ':' << pm2.external_port << std::endl;
+  }
+  catch (std::exception const& e)
+  {
+    output << "  exception: " << e.what() << std::endl;
+  }
+  return sane;
+}
+
+// Return the current user permissions on a given path.
+static
+std::pair<bool, bool>
+permissions(boost::filesystem::path const& path)
+{
+  if (!boost::filesystem::exists(path))
+    throw elle::Error("Doesn't exist");
+  auto s = boost::filesystem::status(path);
+  bool read = s.permissions() & boost::filesystem::perms::owner_read;
+  bool write = s.permissions() & boost::filesystem::perms::owner_write;
+  return std::make_pair(read, write);
+}
+
+// Return the permissions as a string:
+// r, w, rw, None, ...
+static
+std::string
+permissions_string(boost::filesystem::path const& path)
+{
+  try
+  {
+    auto perms = permissions(path);
+    std::string res = "";
+    if (perms.first)
+      res += "r";
+    if (perms.second)
+      res += "w";
+    if (res.length() == 0)
+      res = "None";
+    return res;
+  }
+  catch (...)
+  {
+    return elle::exception_string();
+  }
+}
+
+//
+static
+std::pair<bool, std::string>
+has_permission(boost::filesystem::path const& path,
+               bool mandatory = true)
+{
+  auto res = permissions_string(path);
+  auto good = (res == "rw");
+  auto sane = (good || !mandatory);
+  return std::make_pair(sane, res);
+}
+
+static
+bool
+permission(boost::filesystem::path const& path,
+           bool mandatory = true,
+           uint32_t indent = 0)
+{
+  auto sane = has_permission(path, mandatory);
+  if (script_mode || !sane.first)
+  {
+    auto& output = sane.first ? std::cout : std::cerr;
+    output << std::string(indent, ' ') << path.string() << ": "
+           << (sane.first ? std::string{"ok"} : sane.second) << std::endl;
+  }
+  return sane.first;
+}
+
+static
+bool
+permissions()
+{
+  if (script_mode)
+    std::cout << "Peprmissions:" << std::endl;
+  bool sane = true;
+  sane &= permission(elle::system::home_directory(), true, 2);
+  sane &= permission(infinit::xdg_cache_home(), false, 2);
+  sane &= permission(infinit::xdg_config_home(), false, 2);
+  sane &= permission(infinit::xdg_data_home(), false, 2);
+  sane &= permission(infinit::xdg_state_home(), false, 2);
+  return sane;
+}
+
+static
+bool
+environment()
+{
+  auto env = infinit_related_environment();
+  if (script_mode)
+  {
+    std::cout << "Environment:" << std::endl;
+    for (auto const& entry: env)
+      std::cout << "  " << entry.first << "=" << entry.second << std::endl;
+  }
+  return true;
+}
+
+static
+bool
+space_left(double min_ratio, uint32_t min_absolute)
+{
+  auto f = boost::filesystem::space(infinit::xdg_data_home());
+  double ratio = f.available / (double) f.capacity;
+  bool full = (ratio < min_ratio) || (f.available < min_absolute);
+  if (script_mode || full)
+  {
+    auto& output = full ? std::cerr : std::cout;
+    output << "Space:" << std::endl;
+    output << "  space left: " << f.available << " (" << ratio * 100 << "%)" << std::endl;
+  }
+  return true;
+}
+
+static
+bool
+fuse()
+{
+  try
+  {
+    elle::system::Process p({"fusermount", "-V"});
+    if (script_mode)
+      std::cout << "fuse: ok" << std::endl;
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+static
+bool
+_sanity(boost::program_options::variables_map const& args,
+        boost::optional<std::ostream&> output_stream)
+{
+  bool sane = true;
+  auto test = [] (std::function<void()> const& todo,
+                  std::string message = "") -> bool
+  {
+    try
+    {
+      todo();
+      return true;
+    }
+    catch (...)
+    {
+      if (!message.empty())
+        std::cerr << elle::exception_string() << std::endl;
+      else
+        std::cerr << message << std::endl;
+      return false;
+    }
+  };
+
+  if (script_mode)
+  {
+    std::cout << "Disclaimer: " << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << std::endl;
+  }
+
+  // User name.
+  sane &= test(
+    [&] {
+      auto self_name = self_user_name(args);
+      if (script_mode)
+      {
+        std::cout << "User:" << std::endl;
+        std::cout << "  default user name: " << self_name << std::endl;
+      }
+      // static const boost::regex allowed("${name_regex}");
+      // boost::smatch str_matches;
+      // if (!boost::regex_match(test_name, str_matches, allowed))
+      //   throw elle::Error("You won't be able to push this user on beyond");
+    }, "Invalid or unrecognized user name");
+  sane &= space_left(0.02, 50 * 1024 * 1024);
+  sane &= environment();
+  sane &= permissions();
+  sane &= fuse();
+  return sane;
+}
+
+template <typename T>
+std::map<std::string, std::pair<T, bool>>
+parse(std::vector<T>& container)
+{
+  std::map<std::string, std::pair<T, bool>> output;
+  for (auto& item: container)
+  {
+    auto name = item.name;
+    output.emplace(std::piecewise_construct,
+                   std::forward_as_tuple(name),
+                   std::forward_as_tuple(std::move(item), false));
+  }
+  return output;
+}
+
+template <typename T>
+std::map<std::string, std::pair<std::unique_ptr<T>, bool>>
+parse(std::vector<std::unique_ptr<T>>& container)
+{
+  std::map<std::string, std::pair<std::unique_ptr<T>, bool>> output;
+  for (auto& item: container)
+  {
+    auto name = item->name;
+    output.emplace(std::piecewise_construct,
+                   std::forward_as_tuple(name),
+                   std::forward_as_tuple(std::move(item), false));
+  }
+  return output;
+}
+
+static
+bool
+_integrity(boost::program_options::variables_map const& args,
+           boost::optional<std::ostream&> output_stream)
+{
+  bool sane = true;
+#define CONVERT(xxx, ...)                       \
+  [] () {                                       \
+    auto entities = xxx(__VA_ARGS__);           \
+    return parse(entities);                     \
+  }()
+
+  auto users = CONVERT(ifnt.users_get);
+  auto aws_credentials = ifnt.credentials_aws();
+  auto gcs_credentials = ifnt.credentials_gcs();
+  auto storage_resources = CONVERT(ifnt.storages_get);
+  auto drives = CONVERT(ifnt.drives_get);
+  auto volumes = CONVERT(ifnt.volumes_get);
+  auto networks = CONVERT(ifnt.networks_get);
+  std::cout << "Storage resources:" << std::endl;
+  for (auto& elem: storage_resources)
+  {
+    auto& storage = elem.second.first;
+    auto& status = elem.second.second;
+    if (auto s3config = dynamic_cast<infinit::storage::S3StorageConfig const*>(storage.get()))
+    {
+      auto it =
+        std::find_if(
+          aws_credentials.begin(),
+          aws_credentials.end(),
+          [&s3config] (std::unique_ptr<infinit::AWSCredentials, std::default_delete<infinit::Credentials>> const& credentials)
+          {
+            #define COMPARE(field) (credentials->field == s3config->credentials.field())
+                return COMPARE(access_key_id) && COMPARE(secret_access_key);
+            #undef COMPARE
+          });
+      status = (it != aws_credentials.end());
+      sane &= status;
+      if (script_mode || !status)
+      {
+        auto& output = !status ? std::cerr : std::cout;
+        elle::fprintf(output, "  [%s] %s (S3):\n", result(status), storage->name);
+        if (status)
+          output << "      [✓] Credential: " << (*it)->display_name() << std::endl;
+        else
+          output << "      [×] Missing credential" << std::endl;
+      }
+    }
+    if (auto fsconfig = dynamic_cast<infinit::storage::FilesystemStorageConfig const*>(storage.get()))
+    {
+      status = has_permission(fsconfig->path).first;
+      if (script_mode || !status)
+      {
+        auto& output = !status ? std::cerr : std::cout;
+        elle::fprintf(output, "  [%s] %s (Filesystem):\n", result(status), storage->name);
+        elle::fprintf(output, "      [%s] ", result(has_permission(fsconfig->path, true).first));
+        permission(boost::filesystem::path(fsconfig->path), true);
+      }
+    }
+    if (auto gcsconfig = dynamic_cast<infinit::storage::GCSConfig const*>(storage.get()))
+    {
+      auto it =
+        std::find_if(
+          gcs_credentials.begin(),
+          gcs_credentials.end(),
+          [&gcsconfig] (std::unique_ptr<infinit::OAuthCredentials, std::default_delete<infinit::Credentials>> const& credentials)
+          {
+            return credentials->refresh_token == gcsconfig->refresh_token;
+          });
+      status = (it != gcs_credentials.end());
+      sane &= status;
+      if (script_mode || !status)
+      {
+        auto& output = !status ? std::cerr : std::cout;
+        elle::fprintf(output, "  [%s] %s (GCS):\n", result(status), storage->name);
+        if (status)
+          output << "      [✓] Credential: " << (*it)->display_name() << std::endl;
+        else
+          output << "      [×] Missing credential" << std::endl;
+      }
+    }
+    if (auto ssh = dynamic_cast<infinit::storage::SFTPStorageConfig const*>(storage.get()))
+    {
+      // XXX:
+    }
+  }
+  std::cout << "Networks:" << std::endl;
+  for (auto& elem: networks)
+  {
+    auto const& network = elem.second.first;
+    auto& status = elem.second.second;
+    std::vector<std::string> storage_names;
+    if (network.model)
+    {
+      if (network.model->storage)
+      {
+        if (auto strip = dynamic_cast<infinit::storage::StripStorageConfig*>(
+              network.model->storage.get()))
+          for (auto const& s: strip->storage)
+            storage_names.push_back(s->name);
+        else
+          storage_names.push_back(network.model->storage->name);
+      }
+    }
+    status = storage_names.size() == 0 || std::all_of(
+      storage_names.begin(),
+      storage_names.end(),
+      [&] (std::string const& name) -> bool
+      {
+        auto it = storage_resources.find(name);
+        return (it != storage_resources.end() && it->second.second);
+      });
+    sane &= status;
+    if (script_mode || !status)
+    {
+      auto& output = !status ? std::cerr : std::cout;
+      elle::fprintf(output, "%s[%s] %s:\n", "  ", result(status), network.name);
+      for (auto storage: storage_names)
+      {
+        auto it = storage_resources.find(storage);
+        if (it == storage_resources.end())
+          elle::fprintf(
+            output, "%s    [X] %s: Missing", "  ", storage);
+        else
+          elle::fprintf(
+            output, "%s    [%s] %s", "  ", result(it->second.second), storage);
+        output << std::endl;
+      }
+    }
+  }
+  std::cout << "Volumes:" << std::endl;
+  for (auto& elems: volumes)
+  {
+    auto const& volume = elems.second.first;
+    auto& status = elems.second.second;
+    auto network = networks.find(volume.network);
+    auto network_presents = network != networks.end();
+    status = network_presents && network->second.second;
+    sane &= status;
+    if (script_mode || !status)
+    {
+      auto& output = !status ? std::cerr : std::cout;
+      elle::fprintf(output, "%s[%s] %s:\n%s    [%s] network: %s",
+                    "  ", result(status), volume.name,
+                    "  ", result(status), volume.network);
+      output << std::endl;
+    }
+  }
+  std::cout << "Drives:" << std::endl;
+  for (auto& elems: drives)
+  {
+    auto const& drive = elems.second.first;
+    auto& status = elems.second.second;
+    auto volume = volumes.find(drive.volume);
+    auto volume_presents = volume != volumes.end();
+    auto volume_ok = volume_presents && volume->second.second;
+    auto network = networks.find(drive.network);
+    auto network_presents = network != networks.end();
+    auto network_ok = network_presents && network->second.second;
+    status = network_ok && volume_ok;
+    sane &= status;
+    if (script_mode || !status)
+    {
+      auto& output = !status ? std::cerr : std::cout;
+      elle::fprintf(
+        output, "%s[%s] %s:\n%s    [%s] volume: %s\n%s    [%s] network %s",
+        "  ", result(status), drive.name,
+        "  ", result(volume_ok), drive.volume,
+        "  ", result(network_ok), drive.network
+      );
+      output << std::endl;
+    }
+  }
+  return sane;
+}
+
+COMMAND(integrity)
+{
+  bool sane = true;
+  sane &= _integrity(args, boost::none);
+  if (!sane)
+    throw elle::Error("Please check your configuration");
+}
+
+COMMAND(networking)
+{
+  _networking(args, boost::none);
+}
+
+COMMAND(sanity)
+{
+  bool sane = true;
+  sane &= _sanity(args, boost::none);
+  if (!sane)
+    throw elle::Error("Please check your configuration");
+}
+
+COMMAND(all)
+{
+  bool sane = true;
+  sane &= _sanity(args, boost::none);
+  sane &= _integrity(args, boost::none);
+  sane &= _networking(args, boost::none);
+  if (!sane)
+    throw elle::Error("Please check your configuration");
+}
+
+int
+main(int argc, char** argv)
+{
+  using boost::program_options::value;
+  using boost::program_options::bool_switch;
+  Modes modes {
+    {
+      "all",
+      "Perform all possible checks",
+      &all,
+    },
+    {
+      "networking",
+      "Perform networking checks",
+      &networking,
+    },
+    {
+      "sanity",
+      "Sanity checks",
+      &sanity,
+    },
+    {
+      "integrity",
+      "Integrity checks",
+      &integrity,
+    }
+  };
+  return infinit::main("Infinit diagnostic utility", modes, argc, argv,
+                       std::string("path"), boost::none);
+}
