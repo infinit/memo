d2c978acb3d14d7f13ffd8f13d50b8748f7bf066
mefyl <mefyl@infinit.io>
2016-05-03 16:58:43 +0200

    bin: Factor storage stats reporting errors handling.

--- bin/main.cc
+++ bin/main.cc
@@ -9,29 +9,12 @@ make_stat_update_thread(infinit::User const& self,
                         infinit::Network& network,
                         infinit::model::doughnut::Doughnut& model)
 {
-  model.local()->storage()->register_notifier([&] {
-      try
-      {
-        network.notify_storage(self, model.id());
-      }
-      catch (elle::Error const& e)
-      {
-        ELLE_WARN("Error notifying storage size change: %s", e);
-      }
-    });
-  return reactor::every(
-    60_min,
-    "periodic storage stat updater", [&] {
-      ELLE_TRACE_SCOPE("push storage stats to %s", beyond());
-      try
-      {
-        network.notify_storage(self, model.id());
-      }
-      catch (elle::Error const& e)
-      {
-        ELLE_WARN("Error notifying storage size change: %s", e);
-      }
-    });
+  auto notify = [&]
+    {
+      network.notify_storage(self, model.id());
+    };
+  model.local()->storage()->register_notifier(notify);
+  return reactor::every(60_min, "periodic storage stat updater", notify);
 }
 
 std::string program;
