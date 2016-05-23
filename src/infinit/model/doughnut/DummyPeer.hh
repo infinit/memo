d97e0bb596914031c1f55f4c5c856c0f5ab192b2
mefyl <mefyl@infinit.io>
2016-05-23 15:09:38 +0200

    model.doughnut: Factor make DummyPeer a Paxos::Peer.

--- src/infinit/model/doughnut/DummyPeer.hh
+++ src/infinit/model/doughnut/DummyPeer.hh
@@ -12,36 +12,77 @@ namespace infinit
     namespace doughnut
     {
       class DummyPeer
-        : public Peer
+        : public Paxos::Peer
       {
       public:
         DummyPeer(Address id)
-        : Peer(id)
+          : Peer(id)
         {}
+
         void
         connect(elle::DurationOpt timeout = elle::DurationOpt()) override
-        {
-        }
+        {}
+
         void
         reconnect(elle::DurationOpt timeout = elle::DurationOpt()) override
-        {
-        }
+        {}
+
         void
         store(blocks::Block const& block, StoreMode mode) override
         {
            throw reactor::network::Exception("Peer unavailable");
         }
+
         void
         remove(Address address, blocks::RemoveSignature rs) override
         {
           throw reactor::network::Exception("Peer unavailable");
         }
+
          std::unique_ptr<blocks::Block>
         _fetch(Address address,
                boost::optional<int> local_version) const override
         {
           throw reactor::network::Exception("Peer unavailable");
         }
+
+        virtual
+        boost::optional<PaxosClient::Accepted>
+        propose(PaxosServer::Quorum const& peers,
+                Address address,
+                PaxosClient::Proposal const& p)
+        {
+          throw Paxos::PaxosClient::Peer::Unavailable();
+        }
+
+        virtual
+        PaxosClient::Proposal
+        accept(PaxosServer::Quorum const& peers,
+               Address address,
+               PaxosClient::Proposal const& p,
+               Value const& value)
+        {
+          throw Paxos::PaxosClient::Peer::Unavailable();
+        }
+
+        virtual
+        void
+        confirm(PaxosServer::Quorum const& peers,
+                Address address,
+                PaxosClient::Proposal const& p)
+        {
+
+        }
+
+        virtual
+        boost::optional<PaxosClient::Accepted>
+        get(PaxosServer::Quorum const& peers,
+            Address address,
+            boost::optional<int> local_version)
+        {
+          throw Paxos::PaxosClient::Peer::Unavailable();
+        }
+
         void
         print(std::ostream& stream) const override
         {
@@ -51,8 +92,5 @@ namespace infinit
     }
   }
 }
-
-
-
 
 #endif
