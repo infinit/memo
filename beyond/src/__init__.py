82e016dae6ce7ec1aef09ba2212b7be3e14fa697
Antony MECHIN <antony.mechin@infinit.io>
2016-07-28 10:26:08 +0200

    beyond: Rename owner to owner_name.

--- beyond/src/__init__.py
+++ beyond/src/__init__.py
@@ -219,12 +219,12 @@ class Beyond:
     # Only remove objects owned by the user.
     drives = self.network_drives_get(network = network)
     for d in drives:
-      if d.owner == user.name:
-        self.drive_delete(owner = d.owner, name = d.unqualified_name)
+      if d.owner_name == user.name:
+        self.drive_delete(owner = d.owner_name, name = d.unqualified_name)
     volumes = self.network_volumes_get(network = network)
     for v in volumes:
-      if v.owner == user.name:
-        self.volume_delete(owner = v.owner, name = v.unqualified_name)
+      if v.owner_name == user.name:
+        self.volume_delete(owner = v.owner_name, name = v.unqualified_name)
 
   ## ---- ##
   ## User ##
@@ -265,17 +265,17 @@ class Beyond:
     # Only remove objects owned by the user.
     drives = self.user_drives_get(name = user.name)
     for d in drives:
-      if d.owner == user.name:
-        self.drive_delete(owner = d.owner, name = d.unqualified_name)
+      if d.owner_name == user.name:
+        self.drive_delete(owner = d.owner_name, name = d.unqualified_name)
     volumes = self.user_volumes_get(user = user)
     for v in volumes:
-      if v.owner == user.name:
-        self.volume_delete(owner = v.owner, name = v.unqualified_name)
+      if v.owner_name == user.name:
+        self.volume_delete(owner = v.owner_name, name = v.unqualified_name)
     networks = self.user_networks_get(user = user)
     for n in networks:
-      if n.owner == user.name:
-        self.network_purge(network = network)
-        self.network_delete(owner = n.owner, name = n.unqualified_name)
+      if n.owner_name == user.name:
+        self.network_purge(user = user, network = n)
+        self.network_delete(owner = n.owner_name, name = n.unqualified_name)
 
   ## ------ ##
   ## Volume ##
@@ -296,8 +296,8 @@ class Beyond:
     # Only remove objects owned by the user.
     drives = self.volume_drives_get(name = volume.name)
     for d in drives:
-      if d.owner == user.name:
-        self.drive_delete(owner = d.owner, name = d.unqualified_name)
+      if d.owner_name == user.name:
+        self.drive_delete(owner = d.owner_name, name = d.unqualified_name)
 
   ## ----- ##
   ## Drive ##
@@ -782,7 +782,7 @@ class Network(metaclass = Entity,
     return self.name
 
   @property
-  def owner(self):
+  def owner_name(self):
     return self.name.split('/')[0]
 
   @property
@@ -820,7 +820,7 @@ class Volume(metaclass = Entity,
     return self.name
 
   @property
-  def owner(self):
+  def owner_name(self):
     return self.name.split('/')[0]
 
   @property
@@ -844,7 +844,7 @@ class Drive(
     return self.name
 
   @property
-  def owner(self):
+  def owner_name(self):
     return self.name.split('/')[0]
 
   @property
