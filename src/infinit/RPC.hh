fcd73300350f62fd30ac55881b44bc9cbe89a317
mefyl <mefyl@infinit.sh>
2016-08-23 15:19:47 +0200

    RPC: Remove superfluous log.

--- src/infinit/RPC.hh
+++ src/infinit/RPC.hh
@@ -247,9 +247,7 @@ namespace infinit
       catch (infinit::protocol::Serializer::EOF const&)
       {}
       catch (reactor::network::ConnectionClosed const& e)
-      {
-        ELLE_TRACE("unexpected ConnectionClosed: %s", e.backtrace());
-      }
+      {}
       catch (reactor::network::SocketClosed const& e)
       {
         ELLE_TRACE("unexpected SocketClosed: %s", e.backtrace());
