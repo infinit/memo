30bf4f75f854564a9581919704dfd6e6324c1e43
mefyl <mefyl@infinit.sh>
2016-09-02 12:45:33 +0200

    Change Beyond hostname to beyond.infinit.sh.

--- website/src/__init__.py
+++ website/src/__init__.py
@@ -29,7 +29,7 @@ class Website(bottle.Bottle):
     self.install(bottle.CertificationPlugin())
     route.apply(self)
     self.__swu = sendwithus.api(api_key = 'live_f237084a19cbf6b2373464481155d953a4d86e8d')
-    self.__hub = os.environ.get('INFINIT_BEYOND', 'https://beyond.infinit.io')
+    self.__hub = os.environ.get('INFINIT_BEYOND', 'https://beyond.infinit.sh')
     # self.error_handler = {
     #   404: custom404,
     # }
