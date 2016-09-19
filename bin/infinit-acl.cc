72be18ddc8a151a8a5801fdb3c8fae093c76a9b7
mefyl <mefyl@infinit.sh>
2016-09-19 16:52:52 +0200

    infinit-acl: Workaround GCC 4.9 warning.

--- bin/infinit-acl.cc
+++ bin/infinit-acl.cc
@@ -261,7 +261,10 @@ get_acl(std::string const& path, bool fallback_xattrs)
     buf[sz] = 0;
     std::stringstream ss;
     ss.str(buf);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
     boost::optional<bool> dir_inherit;
+#pragma GCC diagnostic pop
     if (dir)
     {
       int sz = port_getxattr(
