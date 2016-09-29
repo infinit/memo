c1042ed76d0bb7a2c29c28d00d29f3bbf2df5fd4
Christopher Crone <christopher.crone@infinit.sh>
2016-09-29 17:49:44 +0200

    infinit.filesystem.Node: Add get/setxattr for group descriptions.

--- src/infinit/filesystem/Node.cc
+++ src/infinit/filesystem/Node.cc
@@ -399,6 +399,30 @@ namespace infinit
             return;
           }
         }
+        // New naming for group attributes:
+        // infinit.groups.<group_name>.<attribute>
+        else if (special->find("groups.") == 0)
+        {
+          auto name_start = strlen("groups.");
+          auto name_end = special->find_last_of('.');
+          auto group_name = special->substr(name_start, name_end - name_start);
+          model::doughnut::Group group(*dht, group_name);
+          auto attribute = special->substr(name_end + 1);
+          if (attribute == "description")
+          {
+            umbrella([&] {
+              if (v.size())
+                group.set_description(v);
+              else
+                group.set_description(boost::none);
+            });
+            return;
+          }
+          else
+          {
+            elle::err("unknown group attribute: %s", attribute);
+          }
+        }
         throw rfs::Error(ENOATTR, "no such attribute", elle::Backtrace());
       }
       /* Drop quarantine flags, preventing the files from being opened.
@@ -539,10 +563,39 @@ namespace infinit
                 for (auto const& m: members)
                   va.push_back(m->name());
                 o["admins"] = va;
+                try
+                {
+                  if (g.description())
+                    o["description"] = g.description().get();
+                }
+                catch (elle::Error const&)
+                {}
                 std::stringstream ss;
                 elle::json::write(ss, o, true);
                 return ss.str();
               });
+          }
+        }
+        // New naming for group attributes:
+        // infinit.groups.<group_name>.<attribute>
+        else if (special->find("groups.") == 0)
+        {
+          auto name_start = strlen("groups.");
+          auto name_end = special->find_last_of('.');
+          auto group_name = special->substr(name_start, name_end - name_start);
+          model::doughnut::Group group(*dht, group_name);
+          auto attribute = special->substr(name_end + 1);
+          if (attribute == "description")
+          {
+            return umbrella(
+              [&]
+              {
+                return group.description() ? group.description().get() : "";
+              });
+          }
+          else
+          {
+            elle::err("unknown group attribute: %s", attribute);
           }
         }
         else if (special->find("mountpoint") == 0)
