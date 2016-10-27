e1d177506185f446f142b9e63e747dba6c87c550
Matthieu Nottale <matthieu.nottale@infinit.io>
2016-10-27 15:21:21 +0200

    overlay.Kouncil: Add missing include.

--- src/infinit/overlay/kouncil/Kouncil.hh
+++ src/infinit/overlay/kouncil/Kouncil.hh
@@ -1,6 +1,7 @@
 #ifndef INFINIT_OVERLAY_KOUNCIL_HH
 # define INFINIT_OVERLAY_KOUNCIL_HH
 
+# include <random>
 # include <boost/multi_index/hashed_index.hpp>
 # include <boost/multi_index/mem_fun.hpp>
 # include <boost/multi_index/sequenced_index.hpp>
