c13ab6f2bd4d1d32094e3a0ecd45d15f9fd3f8cd
Matthieu Nottale <matthieu.nottale@infinit.sh>
2016-12-01 10:50:02 +0100

    overlay.Kelips: Fix network callback unregistration from destructor.

--- src/infinit/overlay/kelips/Kelips.cc
+++ src/infinit/overlay/kelips/Kelips.cc
@@ -1044,8 +1044,7 @@ namespace infinit
       Node::~Node()
       {
         ELLE_TRACE_SCOPE("%s: destruct", this);
-        if (this->local())
-          this->local()->utp_server()->socket()->unregister_reader("KELIPSGS");
+        this->doughnut()->dock().utp_server().socket()->unregister_reader("KELIPSGS");
         _emitter_thread.reset();
         _pinger_thread.reset();
         this->_state.contacts.clear();
