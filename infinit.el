2f3aa6ca36081c2f432bb5c1c6332a6b2ea7d037
mefyl <mefyl@infinit.sh>
2017-03-01 11:16:25 +0100

    emacs: Update project file.

--- infinit.el
+++ infinit.el
@@ -1,10 +1,7 @@
 (c++-project
  '(
    "src"
-   "elle/das/src"
-   "elle/elle/src"
-   "elle/reactor/src"
-   "elle/cryptography/src"
+   "elle/src"
    )
  "_build/linux64"
  (concat "./drake -j " (int-to-string system-cores-logical) " //build")
