59bb1b302f3b2004d4913ca9c3e3f7716b69ec46
Matthieu Nottale <matthieu.nottale@docker.com>
2017-03-20 11:51:17 +0100

    model.doughnut.Paxos: More balancing by randomizing peer list before sorting.

--- src/infinit/model/doughnut/consensus/Paxos.cc
+++ src/infinit/model/doughnut/consensus/Paxos.cc
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <utility>
+#include <random>
 
 #include <elle/bench.hh>
 #include <elle/memory.hh>
@@ -1630,7 +1631,9 @@ namespace infinit
                 static bool balance = !elle::os::inenv("INFINIT_DISABLE_BALANCED_TRANSFERS");
                 if (balance)
                 {
-                  std::sort(peers.begin(), peers.end(), [this] (auto& p1, auto& p2) ->bool {
+                  static std::default_random_engine gen;
+                  std::shuffle(peers.begin(), peers.end(), gen);
+                  std::stable_sort(peers.begin(), peers.end(), [this] (auto& p1, auto& p2) ->bool {
                     return this->_transfers[p1->id()] < this->_transfers[p2->id()];
                   });
                 }
