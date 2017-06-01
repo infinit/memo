f64609ee6b451bfab7d4fa979a2ea8ace6f77598
Akim Demaille <akim.demaille@docker.com>
2017-06-01 17:28:27 +0200

    crash-report: be sure to create the crash reporter early enough

--- src/infinit/report-crash.cc
+++ src/infinit/report-crash.cc
@@ -12,27 +12,38 @@ ELLE_LOG_COMPONENT("CrashReporter");
 
 namespace infinit
 {
+  using CrashReporter = crash_reporting::CrashReporter;
+
   /// Crash reporter.
-  crash_reporting::CrashReporter
+  auto
   make_reporter()
   {
     auto const host = elle::os::getenv("INFINIT_CRASH_REPORT_HOST", beyond());
     auto const url = elle::sprintf("%s/crash/report", host);
     auto const dumps_path = canonical_folder(xdg_cache_home() / "crashes");
     ELLE_DEBUG("dump to %s", dumps_path);
-    return {url, dumps_path, version_describe()};
+    // FIXME: Should be unique_ptr, but something in our handling of
+    // reactor::Threads prevents it.
+    return std::make_shared<CrashReporter>(url, dumps_path, version_describe());
   }
 
   /// Thread to send the crash reports (some might be pending from
   /// previous runs, saved on disk).
+  ///
+  /// This routine also creates the CrashReporter object which is in
+  /// charge to catching errors and generating the minidumps.  So be
+  /// sure to always create this object early enough.
   std::unique_ptr<elle::reactor::Thread>
   make_reporter_thread()
   {
     ELLE_DEBUG("enabled");
-    return std::make_unique<elle::reactor::Thread>("crash report", [] {
-        auto&& cr = make_reporter();
-        cr.upload_existing();
-      });
+    // Don't create the error handler within the thread, it might not
+    // like that, and might be created too late to catch very early crashes.
+    return std::make_unique<elle::reactor::Thread>("crash report",
+        [cr = make_reporter()]
+        {
+          cr->upload_existing();
+        });
   }
 }
 
