4e41eea0d0961aa3f874d1ddf663d3836d4df8d7
Akim Demaille <akim.demaille@docker.com>
2017-06-02 10:43:42 +0200

    crash-report: tests: be sure to enable crash-reporting when testing it

--- tests/functional/bin/crasher.cc
+++ tests/functional/bin/crasher.cc
@@ -1,5 +1,6 @@
 #include <boost/program_options.hpp>
 
+#include <elle/os/environ.hh>
 #include <elle/reactor/scheduler.hh>
 #include <elle/reactor/Thread.hh>
 
@@ -24,7 +25,7 @@ namespace
   void
   do_crash()
   {
-    int* p = nullptr;
+    auto p = static_cast<int volatile*>(nullptr);
     *p = 1;
   }
 }
@@ -40,30 +41,34 @@ main(int argc, char** argv)
     ("server", po::value<std::string>(), "Server to upload to")
     ("version", po::value<std::string>(), "Version to send to server")
   ;
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
-  if (vm.count("help"))
-  {
-    std::cout << desc << std::endl;
-    return 1;
-  }
-  bool crash = vm.count("crash");
-  auto const dumps = option_str(vm, "dumps");
-  auto const server = option_str(vm, "server");
-  auto const version =
-    vm.count("version") ? option_str(vm, "version") : "test_version";
-  auto crash_reporter =
-    std::make_unique<crash_reporting::CrashReporter>(server, dumps, version);
-  if (crash)
-    do_crash();
-  elle::reactor::Scheduler sched;
-  elle::reactor::Thread main_thread(
-    sched,
-    "main",
-    [&crash_reporter]
+  auto const vm = [&]
     {
-      crash_reporter->upload_existing();
-    });
-  sched.run();
+      auto res = po::variables_map{};
+      po::store(po::parse_command_line(argc, argv, desc), res);
+      po::notify(res);
+      return res;
+    }();
+  if (vm.count("help"))
+    std::cout << desc << std::endl;
+  else
+  {
+    bool crash = vm.count("crash");
+    auto const dumps = option_str(vm, "dumps");
+    auto const server = option_str(vm, "server");
+    auto const version =
+      vm.count("version") ? option_str(vm, "version") : "test_version";
+    // Enable crash handling.
+    elle::os::setenv("INFINIT_CRASH_REPORTER", "1");
+    auto crash_reporter =
+      std::make_unique<crash_reporting::CrashReporter>(server, dumps, version);
+    if (crash)
+      do_crash();
+    elle::reactor::Scheduler sched;
+    elle::reactor::Thread main_thread(sched, "main",
+      [&crash_reporter]
+      {
+        crash_reporter->upload_existing();
+      });
+    sched.run();
+  }
 }
