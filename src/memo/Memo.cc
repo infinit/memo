32ff0c86b2291a08653fa160c1323334626a0cc3
Akim Demaille <akim.demaille@docker.com>
2017-07-17 11:18:53 +0200

    memo: by default, rotate logs at 64GiB
    Discussed with Quentin Hocquet.

--- src/memo/Memo.cc
+++ src/memo/Memo.cc
@@ -44,7 +44,7 @@ namespace memo
     elle::log::logger_add(std::make_unique<elle::log::FileLogger>
        (log_base.string(),
         "*athena*:DEBUG,*cli*:DEBUG,*model*:DEBUG,*grpc*:DEBUG,*prometheus:LOG",
-        1_GiB, 10));
+        64_MiB, 15));
   }
 
   bool
